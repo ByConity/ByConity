19b77dad0c42883adcc98a30c680784b60b5b33a
Yuan Quan <yuanquan.ac@bytedance.com>
2022-01-24 18:23:30 +0800

    fix parse date without delimiter

--- tests/queries/0_stateless/02036_function_date_or_datetime_to.sql
+++ tests/queries/0_stateless/02036_function_date_or_datetime_to.sql
@@ -3,3 +3,9 @@ SELECT toMonth('2021-11-03');
 SELECT toQuarter('2021-11-03');
 SELECT toYear('2021-11-03');
 SELECT toRelativeYearNum('2021-11-03');
+
+SELECT day('20211103');
+SELECT toMonth('20211103');
+SELECT toQuarter('20211103');
+SELECT toYear('20211103');
+SELECT toRelativeYearNum('20211103');
