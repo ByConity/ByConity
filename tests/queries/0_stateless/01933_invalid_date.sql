32e25e7c938089dfb7dec55404b532c51d59db65
Yuan Quan <yuanquan.ac@bytedance.com>
2022-01-24 19:58:22 +0800

    fix testcase

--- tests/queries/0_stateless/01933_invalid_date.sql
+++ tests/queries/0_stateless/01933_invalid_date.sql
@@ -1,7 +1,9 @@
 SELECT toDate('07-08-2019'); -- { serverError 6 }
-SELECT toDate('2019-0708'); -- { serverError 38 }
-SELECT toDate('201907-08'); -- { serverError 38 }
 SELECT toDate('2019^7^8');
+
+-- It can be parsed successfully, but it is not recommended
+SELECT toDate('2019-0708');
+SELECT toDate('201907-08');
 
 CREATE TEMPORARY TABLE test (d Date);
 INSERT INTO test VALUES ('2018-01-01');
