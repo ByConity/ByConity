8f27e38354c1a145688202af0a8d937e61f06556
lta <litianan.2021@bytedance.com>
2022-03-08 11:03:18 +0800

    feat: enrich tests for different data types

--- tests/queries/0_stateless/10066_zookeeper_test_uniquekey_partial_update.sql
+++ tests/queries/0_stateless/10066_zookeeper_test_uniquekey_partial_update.sql
@@ -6,11 +6,12 @@ drop table if exists uniquekey_partial_update1;
 drop table if exists uniquekey_partial_update2;
 
 ------- test normal insert operation
-create table uniquekey_partial_update1 (id Int32, a Int32, b Int32) Engine=HaUniqueMergeTree('/clickhouse/tables/10066_zookeeper_test_uniquekey_partial_update/uniquekey_partial_update', 'r1') order by id unique key id SETTINGS ha_unique_update_log_sleep_ms=10, ha_unique_replay_log_sleep_ms=10, enable_unique_partial_update = 0, enable_unique_row_store = 0;
-create table uniquekey_partial_update2 (id Int32, a Int32, b Int32) Engine=HaUniqueMergeTree('/clickhouse/tables/10066_zookeeper_test_uniquekey_partial_update/uniquekey_partial_update', 'r2') order by id unique key id SETTINGS ha_unique_update_log_sleep_ms=10, ha_unique_replay_log_sleep_ms=10, enable_unique_partial_update = 0, enable_unique_row_store = 0;
+create table uniquekey_partial_update1 (id Int32, a Int32, b LowCardinality(String), c FixedString(10), d Array(Int32), e Map(String, Int32)) Engine=HaUniqueMergeTree('/clickhouse/tables/10066_zookeeper_test_uniquekey_partial_update/uniquekey_partial_update', 'r1') order by id unique key id SETTINGS ha_unique_update_log_sleep_ms=10, ha_unique_replay_log_sleep_ms=10, enable_unique_partial_update = 0, enable_unique_row_store = 0;
+create table uniquekey_partial_update2 (id Int32, a Int32, b LowCardinality(String), c FixedString(10), d Array(Int32), e Map(String, Int32)) Engine=HaUniqueMergeTree('/clickhouse/tables/10066_zookeeper_test_uniquekey_partial_update/uniquekey_partial_update', 'r2') order by id unique key id SETTINGS ha_unique_update_log_sleep_ms=10, ha_unique_replay_log_sleep_ms=10, enable_unique_partial_update = 0, enable_unique_row_store = 0;
 
-insert into uniquekey_partial_update1 values (1, 0, 0), (1, 0, 3), (1, 2, 0), (2, 0, 0), (2, 0, 4), (2, 3, 0);
-insert into uniquekey_partial_update1 (id, b) values (1, 6), (2, 8);
+insert into uniquekey_partial_update1 values 
+(1, 0, 'x', 'x', [1, 2, 3], {'a': 1}), (1, 0, 'z', 'z', [1, 2], {'b': 3, 'c': 4}), (1, 2, 'm', 'm', [], {'a': 2, 'd': 7}), (2, 0, 'q', 'q', [], {'a': 1}), (2, 0, 't', 't', [4, 5, 6], {'e': 8}), (2, 3, '',  '',  [5, 6], {'e': 6, 'f': 9});
+insert into uniquekey_partial_update1 (id, b, e) values (1, 'x', {'h': 12}), (1, '', {'c': 5}), (2, '7', {'f': 10}), (2, '8', {'g': 14});
 select sleep(3) format Null;
 select 'insert with normal mode, using default value';
 select 'select replica 1';
@@ -19,7 +20,7 @@ select 'select replica 2';
 select * from uniquekey_partial_update2;
 
 optimize table uniquekey_partial_update1 final;
-insert into uniquekey_partial_update1 (id, a) values (1, 4), (2, 6);
+insert into uniquekey_partial_update1 (id, a, d) values (1, 4, [10, 20]), (2, 6, [50, 60]);
 select sleep(3) format Null;
 select 'after merge, insert with normal mode';
 select 'select replica 1';
@@ -32,11 +33,12 @@ drop table if exists uniquekey_partial_update2;
 
 select '';
 ------- test partial update insert operation, disable unique_row_store
-create table uniquekey_partial_update1 (id Int32, a Int32, b Int32) Engine=HaUniqueMergeTree('/clickhouse/tables/10066_zookeeper_test_uniquekey_partial_update/uniquekey_partial_update', 'r1') order by id unique key id SETTINGS ha_unique_update_log_sleep_ms=10, ha_unique_replay_log_sleep_ms=10, enable_unique_partial_update = 1, enable_unique_row_store = 0;
-create table uniquekey_partial_update2 (id Int32, a Int32, b Int32) Engine=HaUniqueMergeTree('/clickhouse/tables/10066_zookeeper_test_uniquekey_partial_update/uniquekey_partial_update', 'r2') order by id unique key id SETTINGS ha_unique_update_log_sleep_ms=10, ha_unique_replay_log_sleep_ms=10, enable_unique_partial_update = 1, enable_unique_row_store = 0;
+create table uniquekey_partial_update1 (id Int32, a Int32, b LowCardinality(String), c FixedString(10), d Array(Int32), e Map(String, Int32)) Engine=HaUniqueMergeTree('/clickhouse/tables/10066_zookeeper_test_uniquekey_partial_update/uniquekey_partial_update', 'r1') order by id unique key id SETTINGS ha_unique_update_log_sleep_ms=10, ha_unique_replay_log_sleep_ms=10, enable_unique_partial_update = 1, enable_unique_row_store = 0;
+create table uniquekey_partial_update2 (id Int32, a Int32, b LowCardinality(String), c FixedString(10), d Array(Int32), e Map(String, Int32)) Engine=HaUniqueMergeTree('/clickhouse/tables/10066_zookeeper_test_uniquekey_partial_update/uniquekey_partial_update', 'r2') order by id unique key id SETTINGS ha_unique_update_log_sleep_ms=10, ha_unique_replay_log_sleep_ms=10, enable_unique_partial_update = 1, enable_unique_row_store = 0;
 
-insert into uniquekey_partial_update1 values (1, 0, 0), (1, 0, 3), (1, 2, 0), (2, 0, 0), (2, 0, 4), (2, 3, 0);
-insert into uniquekey_partial_update1 (id, b) values (1, 6), (2, 8);
+insert into uniquekey_partial_update1 values 
+(1, 0, 'x', 'x', [1, 2, 3], {'a': 1}), (1, 0, 'z', 'z', [1, 2], {'b': 3, 'c': 4}), (1, 2, 'm', 'm', [], {'a': 2, 'd': 7}), (2, 0, 'q', 'q', [], {'a': 1}), (2, 0, 't', 't', [4, 5, 6], {'e': 8}), (2, 3, '',  '',  [5, 6], {'e': 6, 'f': 9});
+insert into uniquekey_partial_update1 (id, b, e) values (1, 'x', {'h': 12}), (1, '', {'c': 5}), (2, '7', {'f': 10}), (2, '8', {'g': 14});
 select sleep(3) format Null;
 select 'insert with partial update mode, disable unique row store';
 select 'select replica 1';
@@ -45,7 +47,7 @@ select 'select replica 2';
 select * from uniquekey_partial_update2;
 
 optimize table uniquekey_partial_update1 final;
-insert into uniquekey_partial_update1 (id, a) values (1, 4), (2, 6);
+insert into uniquekey_partial_update1 (id, a, d) values (1, 4, [10, 20]), (2, 6, [50, 60]);
 select sleep(3) format Null;
 select 'after merge, insert with partial update mode, disable unique row store';
 select 'select replica 1';
@@ -58,11 +60,12 @@ drop table if exists uniquekey_partial_update2;
 
 select '';
 ------- test partial update insert operation, enable unique_row_store
-create table uniquekey_partial_update1 (id Int32, a Int32, b Int32) Engine=HaUniqueMergeTree('/clickhouse/tables/10066_zookeeper_test_uniquekey_partial_update/uniquekey_partial_update', 'r1') order by id unique key id SETTINGS ha_unique_update_log_sleep_ms=10, ha_unique_replay_log_sleep_ms=10, enable_unique_partial_update = 1, enable_unique_row_store = 1;
-create table uniquekey_partial_update2 (id Int32, a Int32, b Int32) Engine=HaUniqueMergeTree('/clickhouse/tables/10066_zookeeper_test_uniquekey_partial_update/uniquekey_partial_update', 'r2') order by id unique key id SETTINGS ha_unique_update_log_sleep_ms=10, ha_unique_replay_log_sleep_ms=10, enable_unique_partial_update = 1, enable_unique_row_store = 1;
+create table uniquekey_partial_update1 (id Int32, a Int32, b LowCardinality(String), c FixedString(10), d Array(Int32), e Map(String, Int32)) Engine=HaUniqueMergeTree('/clickhouse/tables/10066_zookeeper_test_uniquekey_partial_update/uniquekey_partial_update', 'r1') order by id unique key id SETTINGS ha_unique_update_log_sleep_ms=10, ha_unique_replay_log_sleep_ms=10, enable_unique_partial_update = 1, enable_unique_row_store = 1;
+create table uniquekey_partial_update2 (id Int32, a Int32, b LowCardinality(String), c FixedString(10), d Array(Int32), e Map(String, Int32)) Engine=HaUniqueMergeTree('/clickhouse/tables/10066_zookeeper_test_uniquekey_partial_update/uniquekey_partial_update', 'r2') order by id unique key id SETTINGS ha_unique_update_log_sleep_ms=10, ha_unique_replay_log_sleep_ms=10, enable_unique_partial_update = 1, enable_unique_row_store = 1;
 
-insert into uniquekey_partial_update1 values (1, 0, 0), (1, 0, 3), (1, 2, 0), (2, 0, 0), (2, 0, 4), (2, 3, 0);
-insert into uniquekey_partial_update1 (id, b) values (1, 6), (2, 8);
+insert into uniquekey_partial_update1 values 
+(1, 0, 'x', 'x', [1, 2, 3], {'a': 1}), (1, 0, 'z', 'z', [1, 2], {'b': 3, 'c': 4}), (1, 2, 'm', 'm', [], {'a': 2, 'd': 7}), (2, 0, 'q', 'q', [], {'a': 1}), (2, 0, 't', 't', [4, 5, 6], {'e': 8}), (2, 3, '',  '',  [5, 6], {'e': 6, 'f': 9});
+insert into uniquekey_partial_update1 (id, b, e) values (1, 'x', {'h': 12}), (1, '', {'c': 5}), (2, '7', {'f': 10}), (2, '8', {'g': 14});
 select sleep(3) format Null;
 select 'insert with partial update mode, enable unique row store';
 select 'select replica 1';
@@ -71,7 +74,7 @@ select 'select replica 2';
 select * from uniquekey_partial_update2;
 
 optimize table uniquekey_partial_update1 final;
-insert into uniquekey_partial_update1 (id, a) values (1, 4), (2, 6);
+insert into uniquekey_partial_update1 (id, a, d) values (1, 4, [10, 20]), (2, 6, [50, 60]);
 select sleep(3) format Null;
 select 'after merge, insert with partial update mode, enable unique row store';
 select 'select replica 1';
