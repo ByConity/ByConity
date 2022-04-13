20352e47e195b92de2205e0825cc330f151959d4
Sumit Sethia <sumit.sethia@bytedance.com>
2022-04-13 11:42:55 +0800

    add minus operator support for DateTime64

---
+++ tests/queries/0_stateless/10082_datetime64_minus_operator.sql
@@ -0,0 +1,3 @@
+select '2020-01-07 05:05:00'::DateTime64 - '2020-01-07 04:05:00'::DateTime64;
+select '2020-01-07 05:05:00'::DateTime64(3, 'UTC') - '2020-01-07 04:05:00'::DateTime64(3, 'Asia/Shanghai');
+select '2020-01-07 05:05:00'::DateTime64(3, 'UTC') - '2020-01-07 04:05:00'::DateTime64(4, 'Asia/Shanghai');
