bb4e9414044dec89a796ecca28ddd86a2e1c1782
guanzhe <guanzhe.andy@bytedance.com>
2022-04-30 10:32:32 +0800

    remove some streams

--- programs/library-bridge/Handlers.cpp
+++ programs/library-bridge/Handlers.cpp
@@ -66,10 +66,14 @@ namespace
 void LibraryRequestHandler::handleRequest(HTTPServerRequest & request, HTTPServerResponse & response)
 {
     LOG_TRACE(log, "Request URI: {}", request.getURI());
+    LOG_TRACE(log, "Ololo");
+    try
+    {
     HTMLForm params(getContext()->getSettingsRef(), request);
-
+    LOG_TRACE(log, "parsed params");
     if (!params.has("method"))
     {
+        LOG_TRACE(log, "No 'method' in request URL");
         processError(response, "No 'method' in request URL");
         return;
     }
@@ -306,6 +310,13 @@ void LibraryRequestHandler::handleRequest(HTTPServerRequest & request, HTTPServe
     catch (...)
     {
         tryLogCurrentException(log);
+    }
+
+    }
+    catch (...)
+    {
+        tryLogCurrentException(log);
+        throw;
     }
 }
 
