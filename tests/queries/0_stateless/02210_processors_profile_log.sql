95a93a495700c4c2ba5478e10588f3aaf456c966
shenziming <shenziming@bytedance.com>
2022-07-21 17:13:40 +0800

    fix 02210 test.

--- tests/queries/0_stateless/02210_processors_profile_log.sql
+++ tests/queries/0_stateless/02210_processors_profile_log.sql
@@ -1,6 +1,7 @@
 -- { echo }
 EXPLAIN PIPELINE SELECT sleep(1);
 
+DROP TABLE IF EXISTS system.query_log;
 DROP TABLE IF EXISTS system.processors_profile_log;
 
 SELECT sleep(1) SETTINGS log_processors_profiles=true, log_queries=1, log_queries_min_type='QUERY_FINISH';
