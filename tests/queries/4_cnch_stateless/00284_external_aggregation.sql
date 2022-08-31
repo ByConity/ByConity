cfcf2f671abad483e6784ade68afb8ad3624f2b6
Đào Minh Thức <minh.dao@bytedance.com>
2022-08-31 03:27:38 +0000

    fix(BYT-0000): fix test case

--- tests/queries/4_cnch_stateless/00284_external_aggregation.sql
+++ tests/queries/4_cnch_stateless/00284_external_aggregation.sql
@@ -1,6 +1,6 @@
-SET enable_optimizer = 0; -- max_bytes_before_external_group_by don't work in optimizer distributed plan.
 SET max_bytes_before_external_group_by = 100000000;
-SET max_memory_usage = 201000000;
+SET max_memory_usage = 351000000;
+set enable_push_partial_agg=0;
 
 SELECT sum(k), sum(c) FROM (SELECT number AS k, count() AS c FROM (SELECT * FROM system.numbers LIMIT 10000000) GROUP BY k);
 SELECT sum(k), sum(c), max(u) FROM (SELECT number AS k, count() AS c, uniqArray(range(number % 16)) AS u FROM (SELECT * FROM system.numbers LIMIT 1000000) GROUP BY k);
