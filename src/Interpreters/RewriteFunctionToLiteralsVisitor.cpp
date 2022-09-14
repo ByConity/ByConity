cc1282ddb996583ab700489b7f1cfd00f0c18bd8
ducle.canh <ducle.canh@bytedance.com>
2022-09-14 10:04:21 +0800

    fix alias in constant folding

--- src/Interpreters/RewriteFunctionToLiteralsVisitor.cpp
+++ src/Interpreters/RewriteFunctionToLiteralsVisitor.cpp
@@ -5,10 +5,16 @@
 
 namespace DB
 {
+/// TODO @canh: more complexed rules can be apply for ast folding here
 void RewriteFunctionToLiteralsMatcher::visit(ASTPtr & ast, Data & /*data*/)
 {
     if (auto * func = ast->as<ASTFunction>())
+    {
         if (auto literal = func->toLiteral())
+        {
+            literal->setAlias(func->tryGetAlias());
             ast = std::move(literal);
+        }
+    }
 }
 }
