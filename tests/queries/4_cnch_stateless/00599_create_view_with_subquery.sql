c3dc041a4e629531187a58a7dfa7b4009da124fb
Vivek <vivek.lakshmanan.v@bytedance.com>
2022-09-30 19:27:32 +0800

    Fix 0599 test case

--- tests/queries/4_cnch_stateless/00599_create_view_with_subquery.sql
+++ tests/queries/4_cnch_stateless/00599_create_view_with_subquery.sql
@@ -3,7 +3,8 @@ DROP TABLE IF EXISTS test_view_00599;
 DROP TABLE IF EXISTS test_00599;
 CREATE TABLE test_00599(id UInt64) ENGINE = CnchMergeTree ORDER BY id;
 CREATE VIEW test_view_00599 AS SELECT * FROM test_00599 WHERE id = (SELECT 1);
-DETACH TABLE test_view_00599 PERMANENTLY; -- { serverError 8053 }
+DETACH TABLE test_view_00599 PERMANENTLY;
+ATTACH TABLE test_view_00599;
 SET show_table_uuid_in_table_create_query_if_not_nil=0;
 SHOW CREATE TABLE test_view_00599;
 DROP TABLE IF EXISTS test_view_00599;
