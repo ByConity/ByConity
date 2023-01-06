27964cae1bda0ecb9fe5aab3f386d90c728e82a9
ducle.canh <ducle.canh@bytedance.com>
2023-01-06 18:41:25 +0800

    remove empty line

--- src/Common/HostWithPorts.cpp
+++ src/Common/HostWithPorts.cpp
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (2022) Bytedance Ltd. and/or its affiliates
  * Licensed under the Apache License, Version 2.0 (the "License");
--- src/Storages/LiveView/StorageLiveView.cpp
+++ src/Storages/LiveView/StorageLiveView.cpp
@@ -1,4 +1,3 @@
-
 /*
  * This file may have been modified by Bytedance Ltd. and/or its affiliates (“ Bytedance's Modifications”).
  * All Bytedance's Modifications are Copyright (2023) Bytedance Ltd. and/or its affiliates.
