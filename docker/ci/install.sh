12d563e7d79035a877047f64991c7bf7d4dea9fb
Dao <minh.dao@bytedance.com>
2023-02-20 10:09:03 +0800

    update Dockerfile for binary builder

---
+++ docker/ci/install.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+bvc_version=1.0.0.94
+tiger_root=/opt/tiger
+bvc_path=$tiger_root/bvc
+export PATH="${PATH}:/${bvc_path}/bin:${tiger_root}/typhoon-blade:${tiger_root}/dev_toolkit"
+
+if ! type '$bvc' > /dev/null; then
+    echo "========== install bvc ============"
+    mkdir -p $bvc_path && cd $bvc_path
+    wget -O bvc.tar.gz http://d.scm.byted.org/api/download/ceph:tao.modules.bvc_$bvc_version.tar.gz
+    tar -xvzf bvc.tar.gz -C $bvc_path
+    echo "========== finish install bvc ============"
+fi
+
+if ! type '$blade' > /dev/null; then
+    set +x
+    echo "========== install blade ============"
+    cd $tiger_root
+    bvc clone inf/blade/blade_build -f typhoon-blade
+    bvc clone dev_toolkit
+    echo "========== finish install blade ============"
+fi
+
+# Donwload HDFS
+cd /
+wget https://downloads.apache.org/hadoop/common/hadoop-3.3.4/hadoop-3.3.4.tar.gz
+tar -xvzf hadoop-3.3.4.tar.gz
+mv hadoop-3.3.4 hadoop
+
+# Setup HDFS config
+cat <<EOT >> ~/.bashrc
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export HADOOP_HOME=/hadoop
+export HADOOP_INSTALL=$HADOOP_HOME
+export HADOOP_MAPRED_HOME=$HADOOP_HOME
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+export YARN_HOME=$HADOOP_HOME
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
+export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/native"
+EOT
+source ~/.bashrc
+echo -e "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64" >> $HADOOP_HOME/etc/hadoop/hadoop-env.sh
+sed '$ i\\t<property><name>fs.defaultFS</name><value>hdfs://127.0.0.1:9110</value></property>' -i $HADOOP_HOME/etc/hadoop/core-site.xml
+sed '$ i\\t<property><name>dfs.replication</name><value>1</value></property>\n\t<property><name>dfs.permissions</name><value>false</value></property>\n\t<property><name>dfs.name.dir</name><value>file:///hadoop/hadoopdata/hdfs/namenode</value></property>\n\t<property><name>dfs.data.dir</name><value>file:///hadoop/hadoopdata/hdfs/datanode</value></property>' -i $HADOOP_HOME/etc/hadoop/hdfs-site.xml
+mkdir -p /hadoop/hadoopdata/hdfs/namenode
+mkdir -p /hadoop/hadoopdata/hdfs/datanode
+
+# Download FDB binaries
+mkdir -p /opt/tiger/foundationdb/bin
+wget https://github.com/apple/foundationdb/releases/download/7.1.0/fdbmonitor.x86_64 -q -O /opt/tiger/foundationdb/bin/fdbmonitor
+wget https://github.com/apple/foundationdb/releases/download/7.1.0/fdbcli.x86_64 -q -O /opt/tiger/foundationdb/bin/fdbcli
+wget https://github.com/apple/foundationdb/releases/download/7.1.0/fdbserver.x86_64 -q -O /opt/tiger/foundationdb/bin/fdbserver
+
