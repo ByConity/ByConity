d8d1a003f09f9e76755e2dfa5811f0b07b9578f0
Vivek <67144643+vivek-debug@users.noreply.github.com>
2023-03-17 08:42:41 +0000

    Enable more test cases

--- tests/queries/4_cnch_stateless/10028_test_func_pathcount_with_optimizer.sql
+++ tests/queries/4_cnch_stateless/10028_test_func_pathcount_with_optimizer.sql
@@ -23,7 +23,7 @@ CREATE TABLE tob_apps
     `user_unique_id` String,
     `event_date` Date,
     `ab_version` Array(Int32),
-    `app_id` UInt32,
+    `tea_app_id` UInt32,
     `app_region` String,
     `region` String,
     `app_language` String,
@@ -35,9 +35,9 @@ CREATE TABLE tob_apps
     `content` String
 )
 ENGINE = CnchMergeTree()
-PARTITION BY (toStartOfDay(`event_date`), `app_id`)
-PRIMARY KEY (`app_id`, `event`, `event_date`, `hash_uid`, `user_unique_id`)
-ORDER BY (`app_id`, `event`, `event_date`, `hash_uid`, `user_unique_id`)
+PARTITION BY (toStartOfDay(`event_date`), `tea_app_id`)
+PRIMARY KEY (`tea_app_id`, `event`, `event_date`, `hash_uid`, `user_unique_id`)
+ORDER BY (`tea_app_id`, `event`, `event_date`, `hash_uid`, `user_unique_id`)
 SETTINGS index_granularity = 8192;
 
 INSERT INTO tob_apps VALUES (0, '11', '22', '33', '44', '55', 0, '11', '22', '33', '44', '55', 1000, 0, 0, '11', '22', '2020.1.1', [1, 2, 3], 0, '', '', '', '', '', '', '', '', '');
@@ -64,7 +64,7 @@ FROM
             (
                 SELECT arrayJoin(pathSplit(600000, 10)(multiIf(server_time < 1609948800, server_time, time > 2000000000, toUInt32(time / 1000), time), multiIf(event = 'goods_pay_success', 1, event = 'goods_pay_success', 2, event = 'goods_pay_success', 3, 0) AS e, '')) AS path
                 FROM tob_apps AS et
-                WHERE (app_id = 173715) AND ((event = 'goods_pay_success') OR (event = 'goods_pay_success') OR (event = 'goods_pay_success')) AND ((event_date >= '2022-06-22') AND (event_date <= '2022-06-28') AND (multiIf(server_time < 1609948800, server_time, time > 2000000000, toUInt32(time / 1000), time) >= 1655827200) AND (multiIf(server_time < 1609948800, server_time, time > 2000000000, toUInt32(time / 1000), time) <= 1656431999))
+                WHERE (tea_app_id = 173715) AND ((event = 'goods_pay_success') OR (event = 'goods_pay_success') OR (event = 'goods_pay_success')) AND ((event_date >= '2022-06-22') AND (event_date <= '2022-06-28') AND (multiIf(server_time < 1609948800, server_time, time > 2000000000, toUInt32(time / 1000), time) >= 1655827200) AND (multiIf(server_time < 1609948800, server_time, time > 2000000000, toUInt32(time / 1000), time) <= 1656431999))
                 GROUP BY hash_uid
             )
         GROUP BY path
