58b63accd42ee916bdc8511b1e73dcbbcb0e1df9
pswai <parksoon_my@hotmail.com>
2023-04-01 08:58:12 +0800

    Add ByConity social card for opengraph (#87)
    * Add ByConity social card for opengraph
    
    * Reduce image size

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/byconity-social-card.jpg",
       navbar: {
         title: "ByConity",
         hideOnScroll: true,
