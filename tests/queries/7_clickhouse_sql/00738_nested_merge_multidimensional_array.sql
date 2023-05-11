11057b34688577c1b168076fe8cbe10f7f08f232
Dao <minh.dao@bytedance.com>
2023-05-11 10:58:29 +0800

    fix test 00738_nested_merge_multidimensional_array

--- tests/queries/7_clickhouse_sql/00738_nested_merge_multidimensional_array.sql
+++ tests/queries/7_clickhouse_sql/00738_nested_merge_multidimensional_array.sql
@@ -1,4 +1,3 @@
-USE test;
 DROP TABLE IF EXISTS sites;
 CREATE TABLE sites (Domain UInt8, `Users.UserID` Array(UInt64), `Users.Dates` Array(Array(Date))) 
 ENGINE = CnchMergeTree ORDER BY Domain SETTINGS vertical_merge_algorithm_min_rows_to_activate = 0, vertical_merge_algorithm_min_columns_to_activate = 0;
@@ -8,6 +7,6 @@ INSERT INTO sites VALUES (2,[1],[['2018-06-22']]);
 SELECT count(), countArray(Users.Dates), countArrayArray(Users.Dates) FROM sites;
 SYSTEM START MERGES sites;
 SELECT sleep(3) FORMAT Null;
-OPTIMIZE TABLE sites FINAL;
+OPTIMIZE TABLE sites;
 SELECT count(), countArray(Users.Dates), countArrayArray(Users.Dates) FROM sites;
 DROP TABLE sites;
