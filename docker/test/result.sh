3c7f0150043c3fa7705b8137f50d48da777ccf4a
Vivek <67144643+vivek-debug@users.noreply.github.com>
2023-05-12 04:06:43 +0000

    fix errors

--- docker/test/result.sh
+++ docker/test/result.sh
@@ -3,12 +3,12 @@
 set -e
 
 #TestSuite Result Analysis
-fail_count=$(cut -f1 hi.tsv | awk '/failed/{++count}')
+fail_count=$(cut -f1 /Artifacts/check_status.tsv | awk '/failed/{++count}')
 if [[ fail_count -eq 0 ]]
 then
     echo "::add-message level=info::all cases pass!"
     exit 0
 else
-    echo "::add-message level=error::$(cat /Artifacts/check_status.tsv)"
+    echo -e "::add-message level=error::\n$(cat /Artifacts/check_status.tsv)"
     exit 1
 fi
