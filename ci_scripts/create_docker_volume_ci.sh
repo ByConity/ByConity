bffb8285441dbba0be8357ee0add0dae7284994d
vivek-debug <vivek.lakshmanan.v@bytedance.com>
2023-06-05 18:09:19 +0800

    fix script

--- ci_scripts/create_docker_volume_ci.sh
+++ ci_scripts/create_docker_volume_ci.sh
@@ -2,13 +2,16 @@ docker container create --name dummy -v my_volume:/opt/byconity/bin/ hello-world
 mv $GITHUB_WORKSPACE/build/programs $GITHUB_WORKSPACE/build/bin
 docker cp $GITHUB_WORKSPACE/build/bin dummy:/opt/byconity/
 docker rm dummy
+mv $GITHUB_WORKSPACE/build/bin $GITHUB_WORKSPACE/build/programs
 
 docker container create --name dummy -v my_hdfs_volume:/etc/hadoop/conf/ hello-world
 mv hdfs conf
 docker cp /CI/conf/ dummy:/etc/hadoop/
 docker rm dummy
+mv conf hdfs
 
 docker container create --name dummy -v my_config_volume:/config hello-world
 mv multi-workers config
 docker cp /CI/config/ dummy:/
 docker rm dummy
+mv config multi-workers
