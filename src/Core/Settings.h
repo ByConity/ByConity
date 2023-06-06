5df8c3eb3084aa5fe24696aa9c28cd1ab1096814
benyucong <benyucong@bytedance.com>
2023-06-06 11:01:55 +0800

    add enable_sql_forwarding to src/Core/Settings.h

--- src/Core/Settings.h
+++ src/Core/Settings.h
@@ -848,6 +848,7 @@ class IColumn;
     M(Bool, enable_low_cardinality_merge_new_algo, true, "Whether use the new merge algorithm during part merge for low cardinality column", 0) \
     M(UInt64, low_cardinality_distinct_threshold, 100000, "Threshold for fallback to native column from low cardinality column, 0 disable", 0) \
     \
+    M(Bool, enable_sql_forwarding, true, "Allow auto query forwarding to target host server.", 1)  \
     M(UInt64, cnch_part_attach_limit, 3000, "Maximum number of part for ATTACH PARTITION/PARTS command", 0)\
     M(UInt64, cnch_part_attach_drill_down, 1, "Maximum levels of path to find cnch data parts, 0 means no drill down", 0) \
     M(UInt64, cnch_part_attach_assert_parts_count, 0, "Assert total number of parts to attach.", 0) \
