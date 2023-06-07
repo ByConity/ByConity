b3e6f030d1bd5f96eb02d02c6e9d2b67e3a56da0
vivek-debug <vivek.lakshmanan.v@bytedance.com>
2023-06-07 17:12:28 +0800

    fix core-site

--- ci_scripts/create_docker_volume_ci.sh
+++ ci_scripts/create_docker_volume_ci.sh
@@ -1,16 +1,13 @@
-docker volume rm ${BINARY_VOL}
 docker container create --name dummy -v ${BINARY_VOL}:/opt/byconity/bin/ hello-world
 docker cp ./bin dummy:/opt/byconity/
 docker rm dummy
 
-docker volume rm ${HDFS_VOL}
 docker container create --name dummy -v ${HDFS_VOL}:/etc/hadoop/conf/ hello-world
 mv hdfs conf
 docker cp /CI/conf/ dummy:/etc/hadoop/
 docker rm dummy
 mv conf hdfs
 
-docker volume rm ${CONFIG_VOL}
 docker container create --name dummy -v ${CONFIG_VOL}:/config hello-world
 mv multi-workers config
 docker cp /CI/config/ dummy:/
