b2a59e63c5d7a6c9ca136f902c57eb8fd2a74f54
vivek-debug <vivek.lakshmanan.v@bytedance.com>
2023-06-07 18:08:25 +0800

    use - instead of _

--- docker/local-deploy/hdfs/create_users.sh
+++ docker/local-deploy/hdfs/create_users.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e -x
-docker exec ${COMPOSE_PROJECT_NAME}_hdfs-namenode hdfs dfs -mkdir -p /user/clickhouse
-docker exec ${COMPOSE_PROJECT_NAME}_hdfs-namenode hdfs dfs -chown clickhouse /user/clickhouse
-docker exec ${COMPOSE_PROJECT_NAME}_hdfs-namenode hdfs dfs -chmod -R 775 /user/clickhouse 
+docker exec ${COMPOSE_PROJECT_NAME}-hdfs-namenode hdfs dfs -mkdir -p /user/clickhouse
+docker exec ${COMPOSE_PROJECT_NAME}-hdfs-namenode hdfs dfs -chown clickhouse /user/clickhouse
+docker exec ${COMPOSE_PROJECT_NAME}-hdfs-namenode hdfs dfs -chmod -R 775 /user/clickhouse 
