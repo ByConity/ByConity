2adbf467e4ec1f0fe27c8a1d634b9495764679c2
vivek-debug <vivek.lakshmanan.v@bytedance.com>
2023-06-07 13:13:58 +0800

    update CI with unique env vars

--- ci_scripts/create_docker_volume_ci.sh
+++ ci_scripts/create_docker_volume_ci.sh
@@ -1,17 +1,17 @@
-docker volume rm my_volume
-docker container create --name dummy -v my_volume:/opt/byconity/bin/ hello-world
+docker volume rm ${BINARY_VOL}
+docker container create --name dummy -v ${BINARY_VOL}:/opt/byconity/bin/ hello-world
 docker cp ./bin dummy:/opt/byconity/
 docker rm dummy
 
-docker volume rm my_hdfs_volume
-docker container create --name dummy -v my_hdfs_volume:/etc/hadoop/conf/ hello-world
+docker volume rm ${HDFS_VOL}
+docker container create --name dummy -v ${HDFS_VOL}:/etc/hadoop/conf/ hello-world
 mv hdfs conf
 docker cp /CI/conf/ dummy:/etc/hadoop/
 docker rm dummy
 mv conf hdfs
 
-docker volume rm my_config_volume
-docker container create --name dummy -v my_config_volume:/config hello-world
+docker volume rm ${CONFIG_VOL}
+docker container create --name dummy -v ${CONFIG_VOL}:/config hello-world
 mv multi-workers config
 docker cp /CI/config/ dummy:/
 docker rm dummy
