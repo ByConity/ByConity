9ead634770589ffc4fdaad9edb18c514219d26ba
benyucong <benyucong@bytedance.com>
2023-06-08 15:52:56 +0800

    Merge branch 'adb-number-functions' into 'cnch-ce-merge'
    feat(clickhousech@m-10517770): ADB Compatibility - Cherry picking number functions to CNCH-CE-Merge
    
    See merge request dp/ClickHouse!12997

---
+++ src/Functions/cot.cpp
@@ -0,0 +1,29 @@
+#include <DataTypes/IDataType.h>
+#include <Functions/FunctionFactory.h>
+#include <Functions/FunctionMathUnary.h>
+
+
+namespace DB
+{
+namespace
+{
+    struct CotName
+    {
+        static constexpr auto name = "cot";
+    };
+
+    Float64 cot(Float64 r)
+    {
+        Float64 cot = 1.0 / tan(r);
+        return cot;
+    }
+
+    using FunctionCot = FunctionMathUnary<UnaryFunctionVectorized<CotName, cot>>;
+}
+
+void registerFunctionCot(FunctionFactory & factory)
+{
+    factory.registerFunction<FunctionCot>(FunctionFactory::CaseInsensitive);
+}
+
+}
