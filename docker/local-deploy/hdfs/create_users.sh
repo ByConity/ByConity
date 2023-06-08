fa53b28087861b6b45f5bc679039c9c16a5bf290
vivek-debug <vivek.lakshmanan.v@bytedance.com>
2023-06-08 16:47:48 +0800

    Move to ci-deploy folder

--- docker/local-deploy/hdfs/create_users.sh
+++ docker/local-deploy/hdfs/create_users.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e -x
-docker exec ${COMPOSE_PROJECT_NAME}-hdfs-namenode hdfs dfs -mkdir -p /user/clickhouse
-docker exec ${COMPOSE_PROJECT_NAME}-hdfs-namenode hdfs dfs -chown clickhouse /user/clickhouse
-docker exec ${COMPOSE_PROJECT_NAME}-hdfs-namenode hdfs dfs -chmod -R 775 /user/clickhouse 
+docker exec hdfs-namenode hdfs dfs -mkdir /user
+docker exec hdfs-namenode hdfs dfs -mkdir /user/clickhouse
+docker exec hdfs-namenode hdfs dfs -chown clickhouse /user/clickhouse
+docker exec hdfs-namenode hdfs dfs -chmod -R 775 /user/clickhouse 
