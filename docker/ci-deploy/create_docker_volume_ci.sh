aafea157ae6b52ba40cb23f332a8c82901777be7
vivek-debug <vivek.lakshmanan.v@bytedance.com>
2023-06-08 16:53:12 +0800

    more rename and refactor

--- docker/ci-deploy/create_docker_volume_ci.sh
+++ docker/ci-deploy/create_docker_volume_ci.sh
@@ -7,7 +7,5 @@ docker cp /CI/conf/ dummy:/etc/hadoop/
 docker rm dummy
 
 docker container create --name dummy -v ${CONFIG_VOL}:/config hello-world
-mv multi-workers config
 docker cp /CI/config/ dummy:/
 docker rm dummy
-mv config multi-workers
