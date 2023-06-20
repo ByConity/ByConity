726b814e60d8c8f9e438385491fc2d4d602c59dd
benyucong <benyucong@gmail.com>
2023-06-20 10:41:38 +0800

    key1, key2

--- tests/queries/4_cnch_stateless/10062_nestloop_join_use_using.sql
+++ tests/queries/4_cnch_stateless/10062_nestloop_join_use_using.sql
@@ -5,8 +5,8 @@ SET enable_shuffle_with_order = 1;
 DROP TABLE IF EXISTS nse_lhs;
 DROP TABLE IF EXISTS nse_rhs;
 
-CREATE TABLE nse_lhs (key1 int, value Nullable(UInt8)) ENGINE=CnchMergeTree() order by key;
-CREATE TABLE nse_rhs (key2 int, value Nullable(UInt8)) ENGINE=CnchMergeTree() order by key;
+CREATE TABLE nse_lhs (key1 int, value Nullable(UInt8)) ENGINE=CnchMergeTree() order by key1;
+CREATE TABLE nse_rhs (key2 int, value Nullable(UInt8)) ENGINE=CnchMergeTree() order by key2;
 
 INSERT INTO nse_lhs VALUES (1,1) (2, 2) (3, NULL) (4, NULL) (5,6) (6, NULL);
 INSERT INTO nse_rhs VALUES (1,1) (2, NULL) (3, 2) (4, NULL) (5,7) (6, 0);
