91803feb8fbdf0b275c5860d04025becabe046df
benyucong <benyucong@gmail.com>
2023-06-21 11:12:17 +0800

    sync src/Parsers/ASTSystemQuery.cpp with internal

--- src/Parsers/ASTSystemQuery.cpp
+++ src/Parsers/ASTSystemQuery.cpp
@@ -1,24 +1,3 @@
-/*
- * Copyright 2016-2023 ClickHouse, Inc.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-
-/*
- * This file may have been modified by Bytedance Ltd. and/or its affiliates (“ Bytedance's Modifications”).
- * All Bytedance's Modifications are Copyright (2023) Bytedance Ltd. and/or its affiliates.
- */
-
 #include <Parsers/IAST.h>
 #include <Parsers/ASTSystemQuery.h>
 #include <Common/quoteString.h>
@@ -184,6 +163,10 @@ const char * ASTSystemQuery::typeToString(Type type)
             return "START DEDUP WORKER";
         case Type::STOP_DEDUP_WORKER:
             return "STOP DEDUP WORKER";
+        case Type::START_CLUSTER:
+            return "START CLUSTER";
+        case Type::STOP_CLUSTER:
+            return "STOP CLUSTER";
         case Type::DUMP_SERVER_STATUS:
             return "DUMP SERVER STATUS";
         case Type::UNKNOWN:
