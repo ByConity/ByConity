abe277ee9c639241ffc17d42631d86e99ba66db3
benyucong <benyucong@gmail.com>
2023-06-23 15:05:02 +0800

    fix Context.cpp and Context.h

--- src/Interpreters/Context.cpp
+++ src/Interpreters/Context.cpp
@@ -4310,24 +4310,6 @@ void Context::controlCnchBGThread(const StorageID & storage_id, CnchBGThreadType
     getCnchBGThreadsMap(type)->controlThread(storage_id, action);
 }
 
-bool Context::getTableReclusterTaskStatus(const StorageID & storage_id) const
-{
-    CnchBGThreadsMap * thread_map = getCnchBGThreadsMap(CnchBGThreadType::Clustering);
-    if (!thread_map)
-        throw Exception("Fail to get merge thread map", ErrorCodes::SYSTEM_ERROR);
-    CnchBGThreadPtr bg_thread_ptr = thread_map->tryGetThread(storage_id);
-    if (!bg_thread_ptr)
-    {
-        LOG_DEBUG(&Poco::Logger::get(__PRETTY_FUNCTION__), "Fail to get reclustering manager thread for " + storage_id.getNameForLogs());
-        return false;
-    }
-
-    ReclusteringManagerThread * reclustering_manager_thread = dynamic_cast<ReclusteringManagerThread *>(bg_thread_ptr.get());
-    if (!reclustering_manager_thread)
-        throw Exception("Fail to cast to ReclusteringManagerThread", ErrorCodes::LOGICAL_ERROR);
-    return reclustering_manager_thread->getTableReclusterStatus();
-}
-
 bool Context::removeMergeMutateTasksOnPartitions(const StorageID & storage_id, const std::unordered_set<String> & partitions)
 {
     CnchBGThreadsMap * thread_map = getCnchBGThreadsMap(CnchBGThreadType::MergeMutate);
