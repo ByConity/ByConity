6354fa05f9ed8f2a119ea1a0ed53fa520ee2f760
Dao <minh.dao@bytedance.com>
2023-07-04 08:24:27 +0800

    update test

--- tests/queries/4_cnch_stateless/50001_test_query_cache.sql
+++ tests/queries/4_cnch_stateless/50001_test_query_cache.sql
@@ -5,31 +5,23 @@ CREATE TABLE test_query_cache (d Date, id UInt64, a String)
     ORDER BY `id`
     SAMPLE BY `id`;
 
+--- miss, hit, hit again, miss, hit, hit again ---
 INSERT INTO test_query_cache values ('2019-01-01', 1, 'a');
---- miss ---
 sELECT * FROM test_query_cache SETTINGS use_query_cache = 1; 
 
---- hit ---
 SeLECT * FROM test_query_cache SETTINGS use_query_cache = 1; 
 
---- hit again ---
 SElECT * FROM test_query_cache SETTINGS use_query_cache = 1; 
 
 INSERT INTO test_query_cache values ('2019-01-01', 2, 'a')
---- miss ---
 SELeCT * FROM test_query_cache ORDER BY id SETTINGS use_query_cache = 1; 
 
---- hit ---
 SELEcT * FROM test_query_cache ORDER BY id SETTINGS use_query_cache = 1; 
 
---- hit again ---
 SELECt * FROM test_query_cache ORDER BY id SETTINGS use_query_cache = 1; 
 DROP TABLE test_query_cache;
 --- wait until data is flush into system table --
-SELECT sleep(3) Format Null;
-SELECT sleep(3) Format Null;
-SELECT sleep(3) Format Null;
-SELECT sleep(3) Format Null;
+SYSTEM FLUSH LOGS;
 SELECT ProfileEvents.Values[indexOf(ProfileEvents.Names, 'QueryCacheHits')] as h, ProfileEvents.Values[indexOf(ProfileEvents.Names, 'QueryCacheMisses')] as m FROM cnch(server, system.query_log) WHERE event_date = today() and type = 'QueryFinish' and query = 'sELECT * FROM test_query_cache SETTINGS use_query_cache = 1;';
 
 SELECT ProfileEvents.Values[indexOf(ProfileEvents.Names, 'QueryCacheHits')] as h, ProfileEvents.Values[indexOf(ProfileEvents.Names, 'QueryCacheMisses')] as m FROM cnch(server, system.query_log) WHERE event_date = today() and type = 'QueryFinish' and query = 'SeLECT * FROM test_query_cache SETTINGS use_query_cache = 1;';
--- tests/queries/4_cnch_stateless/50001_test_query_cache_with_optimizer.sql
+++ tests/queries/4_cnch_stateless/50001_test_query_cache_with_optimizer.sql
@@ -8,44 +8,35 @@ CREATE TABLE right_table (d Date, id UInt64, a String)
     PARTITION BY `d`
     ORDER BY `id`;
 
+--- miss, hit, hit again,  ---
 INSERT INTO left_table values ('2019-01-01', 1, 'a');
 INSERT INTO right_table values ('2019-01-01', 1, 'b');
---- miss ---
 sELECT a, r.a from left_table as l INNER JOIN right_table as r on l.id = r.id ORDER BY id Settings use_query_cache = 1, enable_optimizer = 1;
 
---- hit ---
 SeLECT a, r.a from left_table as l INNER JOIN right_table as r on l.id = r.id ORDER BY id Settings use_query_cache = 1, enable_optimizer = 1; 
 
---- hit again ---
 SElECT a, r.a from left_table as l INNER JOIN right_table as r on l.id = r.id ORDER BY id Settings use_query_cache = 1, enable_optimizer = 1; 
 
+--- miss, hit, hit again ---
 INSERT INTO left_table values ('2019-01-01', 2, 'c');
---- miss ---
 SELeCT a, r.a from left_table as l INNER JOIN right_table as r on l.id = r.id ORDER BY id Settings use_query_cache = 1, enable_optimizer = 1; 
 
---- hit ---
 SELEcT a, r.a from left_table as l INNER JOIN right_table as r on l.id = r.id ORDER BY id Settings use_query_cache = 1, enable_optimizer = 1; 
 
---- hit again ---
 SELECt a, r.a from left_table as l INNER JOIN right_table as r on l.id = r.id ORDER BY id Settings use_query_cache = 1, enable_optimizer = 1; 
 
+--- miss, hit, hit again --
 INSERT INTO right_table values ('2019-01-01', 2, 'd');
---- miss --
 SELECT a, r.a From left_table as l INNER JOIN right_table as r on l.id = r.id ORDER BY id Settings use_query_cache = 1, enable_optimizer = 1; 
 
---- hit ---
 SELECT a, r.a fRom left_table as l INNER JOIN right_table as r on l.id = r.id ORDER BY id Settings use_query_cache = 1, enable_optimizer = 1; 
 
---- hit again ---
 SELECT a, r.a frOm left_table as l INNER JOIN right_table as r on l.id = r.id ORDER BY id Settings use_query_cache = 1, enable_optimizer = 1; 
 
 DROP TABLE left_table;
 DROP TABLE right_table;
 --- wait until data is flush into system table --
-SELECT sleep(3) Format Null;
-SELECT sleep(3) Format Null;
-SELECT sleep(3) Format Null;
-SELECT sleep(3) Format Null;
+SYSTEM FLUSH LOGS;
 SELECT ProfileEvents.Values[indexOf(ProfileEvents.Names, 'QueryCacheHits')] as h, ProfileEvents.Values[indexOf(ProfileEvents.Names, 'QueryCacheMisses')] as m FROM cnch(server, system.query_log) WHERE event_date = today() and type = 'QueryFinish' and query = 'sELECT a, r.a from left_table as l INNER JOIN right_table as r on l.id = r.id ORDER BY id Settings use_query_cache = 1, enable_optimizer = 1;';
 
 SELECT ProfileEvents.Values[indexOf(ProfileEvents.Names, 'QueryCacheHits')] as h, ProfileEvents.Values[indexOf(ProfileEvents.Names, 'QueryCacheMisses')] as m FROM cnch(server, system.query_log) WHERE event_date = today() and type = 'QueryFinish' and query = 'SeLECT a, r.a from left_table as l INNER JOIN right_table as r on l.id = r.id ORDER BY id Settings use_query_cache = 1, enable_optimizer = 1;';
