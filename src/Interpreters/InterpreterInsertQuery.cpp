856d0c2bec4c564a6c582998e93b0c5f0d7df314
王思远 <wangsiyuan.1008@bytedance.com>
2023-07-06 02:29:48 +0000

    Merge branch '2.0_s3_infile_outfile' into 'cnch-ce-merge'
    fix(clickhousech@m-13699796): infile&outfile support for s3
    
    See merge request dp/ClickHouse!14351

--- src/Interpreters/InterpreterInsertQuery.cpp
+++ src/Interpreters/InterpreterInsertQuery.cpp
@@ -38,8 +38,9 @@
 #include <DataTypes/DataTypeNullable.h>
 #include <IO/ConnectionTimeoutsContext.h>
 #include <IO/SnappyReadBuffer.h>
-#include <IO//RAReadBufferFromS3.h>
 #include <Interpreters/Context.h>
+#include <IO/S3Common.h>
+#include <Storages/S3/RAReadBufferFromS3.h>
 #include <Interpreters/InterpreterSelectWithUnionQuery.h>
 #include <Interpreters/InterpreterWatchQuery.h>
 #include <Interpreters/QueryLog.h>
@@ -697,6 +698,21 @@ BlockInputStreamPtr InterpreterInsertQuery::buildInputStreamFromSource(
                         nullptr,
                         0,
                         false,
+                        context_ptr->getProcessList().getHDFSDownloadThrottler());
+                }
+#endif
+#if USE_AWS_S3
+                else if (isS3URIScheme(scheme))
+                {
+                    S3::URI s3_uri(Poco::URI(uriPrefix + name));
+                    String endpoint = s3_uri.endpoint.empty() ? context_ptr->getSettingsRef().s3_endpoint.toString() : s3_uri.endpoint;
+                    String bucket = s3_uri.bucket;
+                    String key = s3_uri.key;
+                    S3::S3Config s3_cfg(endpoint, context_ptr->getSettingsRef().s3_region.toString(), bucket,
+                        context_ptr->getSettingsRef().s3_ak_id.toString(), context_ptr->getSettingsRef().s3_ak_secret.toString(), "");
+                    const std::shared_ptr<Aws::S3::S3Client> client = s3_cfg.create();
+                    read_buf = std::make_unique<RAReadBufferFromS3>(client, bucket, key, 3,
+                        DBMS_DEFAULT_BUFFER_SIZE, nullptr, 0,
                         context_ptr->getProcessList().getHDFSDownloadThrottler());
                 }
 #endif
