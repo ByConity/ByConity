0ef25fd327827b1701a0e9a75dfcaebcda3a58b3
benyucong <benyucong@gmail.com>
2023-07-07 10:27:08 +0800

    modify wrongly cherry-pick in src/Core/Settings.h

--- src/Core/Settings.h
+++ src/Core/Settings.h
@@ -1919,7 +1919,8 @@ class IColumn;
       FormatSettings::DateTimeOutputFormat::Simple, \
       "Method to write DateTime to text output. Possible values: 'simple', 'iso', 'unix_timestamp'.", \
       0) \
-\
+    M(String, bool_true_representation, "true", "Text to represent bool value in TSV/CSV formats.", 0) \
+    M(String, bool_false_representation, "false", "Text to represent bool value in TSV/CSV formats.", 0) \
     M(UInt64, max_hdfs_write_buffer_size, DBMS_DEFAULT_BUFFER_SIZE, "The maximum size of the buffer to write data to hdfs.", 0) \
 \
     M(Bool, \
@@ -2044,8 +2045,8 @@ class IColumn;
     M(UInt64, cnch_part_attach_max_threads, 16, "Max threads to use when attach parts", 0) \
     M(UInt64, attach_failure_injection_knob, 0, "Attach failure injection knob, for test only", 0) \
     M(Bool, async_post_commit, false, "Txn post commit asynchronously", 0) \
-    M(Bool, enable_auto_query_forwarding, false, "Auto forward query to target server when having multiple servers", 0)\
-    M(String, tenant_id, "", "tenant_id of cnch user", 0) \
+    M(Bool, enable_auto_query_forwarding, false, "Auto forward query to target server when having multiple servers", 0) \
+    M(String, tenant_id, "", "tenant_id of cnch user", 0)
 
 
 // End of FORMAT_FACTORY_SETTINGS
