6526c76608461cde6f7ec7bb0df94ea2279a0d69
benyucong <benyucong@gmail.com>
2023-07-12 13:42:18 +0800

    Merge branch 'adb-date' into 'cnch-ce-merge'
    feat(clickhousech@m-12861763): ADB Compatibility - date functions for CNCH 2.0
    
    See merge request dp/ClickHouse!13488
    # Conflicts:
    #	src/Functions/formatDateTime.cpp

---
+++ tests/queries/4_cnch_stateless/01523_period_diff_test.sql
@@ -0,0 +1,24 @@
+select period_diff(202202,202103);
+select period_diff('202202','202103');
+select period_diff(202202,'202103');
+select period_diff('202202',202103);
+select period_diff(999912,000000);
+select period_diff(000000,999912);
+select period_diff(000000,-000001); -- { serverError 377 }
+
+CREATE DATABASE IF NOT EXISTS test;
+DROP TABLE IF EXISTS test.test_period_diff;
+CREATE TABLE test.test_period_diff (
+  id Int64,
+  date1 String,
+  date2 String,
+  date3 Int64
+)
+ENGINE = CnchMergeTree()
+ORDER BY id;
+
+
+INSERT INTO test.test_period_diff VALUES (0, '202202', '202103', 202103), (1, '999912', '000000', 000000);
+SELECT id, period_diff(date1, date2) FROM test.test_period_diff ORDER BY id;
+SELECT id, period_diff(date1, date3) FROM test.test_period_diff ORDER BY id;
+DROP TABLE IF EXISTS test.test_period_diff;
---
+++ tests/queries/4_cnch_stateless/42222_from_days_and_to_days_with_string_literal.sql
@@ -0,0 +1,1 @@
+SELECT from_days('0'), from_days('2147483647'), from_days('719528'), from_days(CAST(1000000000 AS Int64)), from_days(NULL);
