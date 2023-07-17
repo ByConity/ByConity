f2bf118a28472c5f5bd188e6c320d64ca2680f50
benyucong <benyucong@gmail.com>
2023-07-17 11:41:46 +0800

    fix wrongly picked codes

--- src/Common/HostWithPorts.h
+++ src/Common/HostWithPorts.h
@@ -210,6 +210,27 @@ public:
 };
 
 std::ostream & operator<<(std::ostream & os, const HostWithPorts & host_ports);
+using String = std::string;
+struct WorkerId
+{
+    WorkerId(const String & vw_name_, const String & wg_name_, const String & id_) : vw_name(vw_name_), wg_name(wg_name_), id(id_) { }
+    WorkerId() = default;
+    String vw_name;
+    String wg_name;
+    String id;
+    const String ToString() const;
+};
+struct WorkerIdEqual
+{
+    bool operator()(const WorkerId & lhs, const WorkerId & rhs) const
+    {
+        return lhs.vw_name == rhs.vw_name && lhs.wg_name == rhs.wg_name && lhs.id == rhs.id;
+    }
+};
+struct WorkerIdHash
+{
+    std::size_t operator()(const WorkerId & worker_id) const { return std::hash<String>()(worker_id.ToString()); }
+};
 }
 
 namespace std
