2024572259950af70150fcc1ee5e54ce956e67ae
benyucong <benyucong@gmail.com>
2023-07-18 10:51:33 +0800

    copy putTTL function

--- src/Catalog/MetastoreProxy.cpp
+++ src/Catalog/MetastoreProxy.cpp
@@ -13,18 +13,17 @@
  * limitations under the License.
  */
 
-#include <Catalog/MetastoreProxy.h>
-#include <Protos/DataModelHelpers.h>
 #include <cstddef>
 #include <random>
 #include <sstream>
 #include <vector>
 #include <string.h>
+#include <Catalog/MetastoreProxy.h>
 #include <DaemonManager/BGJobStatusInCatalog.h>
 #include <IO/ReadHelpers.h>
 #include <Protos/DataModelHelpers.h>
 #include "common/types.h"
-#include "Catalog/MetastoreByteKVImpl.h"
+#include "Catalog/MetastoreFDBImpl.h"
 
 namespace DB::ErrorCodes
 {
@@ -1831,9 +1830,9 @@ UInt64 MetastoreProxy::getMergeMutateThreadStartTime(const String & name_space, 
 void MetastoreProxy::setAsyncQueryStatus(
     const String & name_space, const String & id, const Protos::AsyncQueryStatus & status, UInt64 ttl) const
 {
-    if (auto * bytekv = dynamic_cast<MetastoreByteKVImpl *>(metastore_ptr.get()))
+    if (auto * fdkv = dynamic_cast<MetastoreFDBImpl *>(metastore_ptr.get()))
     {
-        bytekv->putTTL(asyncQueryStatusKey(name_space, id), status.SerializeAsString(), ttl);
+        fdkv->putTTL(asyncQueryStatusKey(name_space, id), status.SerializeAsString(), ttl);
         return;
     }
     metastore_ptr->put(asyncQueryStatusKey(name_space, id), status.SerializeAsString());
