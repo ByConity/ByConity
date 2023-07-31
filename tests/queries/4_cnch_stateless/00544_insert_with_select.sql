b0b26b08ee65af26fc819b46086ea5359ecc7880
ByConityBot <benyucong@bytedance.com>
2023-07-31 16:27:22 +0800

    enable optimizer

--- tests/queries/4_cnch_stateless/00544_insert_with_select.sql
+++ tests/queries/4_cnch_stateless/00544_insert_with_select.sql
@@ -2,6 +2,8 @@ DROP TABLE IF EXISTS test;
 
 CREATE TABLE test(number UInt64, num2 UInt64) ENGINE = CnchMergeTree() ORDER BY number;
 
+SET enable_optimizer = 1;
+
 INSERT INTO test WITH number * 2 AS num2 SELECT number, num2 FROM system.numbers LIMIT 3;
 
 SELECT * FROM test;
