2fdb92f2334b159f90d4c567cba03afe63c90b33
ByConityBot <benyucong@bytedance.com>
2023-08-21 17:35:49 +0800

    Merge branch 'cnch_ce_func' into 'cnch-ce-merge'
    fix(clickhousech@m-13492441): function get_json_object
    
    See merge request dp/ClickHouse!14573

---
+++ tests/queries/4_cnch_stateless/12294_function_get_json_object.sql
@@ -0,0 +1,13 @@
+SELECT get_json_object('{"n_s" : [{"ac":"abc","xz":"xz"}, {"def":"def"}], "n_i" : [1, 23]}', '$.n_s[0].ac');
+SELECT get_json_object('{"n_s" : [{"ac":"abc","xz":"xz"}, {"def":"def"}], "n_i" : [1, 23]}', '$.n_s[0]');
+SELECT get_json_object('{"n_s" : [{"ac":"abc","xz":"xz"}, {"def":"def"}], "n_i" : [1, 23]}', '$.n_s');
+SELECT JSONExtractRaw('{"n_s" : [{"ac":"abc","xz":"xz"}, {"def":"def"}], "n_i" : [1, 23]}', 'n_s', 1, 'ac');
+SELECT JSONExtractRaw('{"n_s" : [{"ac":"abc","xz":"xz"}, {"def":"def"}], "n_i" : [1, 23]}', 'n_s', 1);
+SELECT JSONExtractRaw('{"n_s" : [{"ac":"abc","xz":"xz"}, {"def":"def"}], "n_i" : [1, 23]}', 'n_s');
+
+DROP TABLE IF EXISTS test.test;
+CREATE TABLE test.test(a Nullable(String)) ENGINE = CnchMergeTree ORDER BY tuple() PARTITION BY tuple();
+SELECT get_json_object('{"test": "test"}', a) FROM test.test;
+INSERT INTO test.test values('{"d" : "2017-08-31 18:36:48", "t" : {"a": "b"}}'), ('{"d" : "1504193808", "t" : -1}'), ('{"d" : 1504193808, "t" : ["a", "c"]}'), ('{"d" : false, "t" : [{"a":"c"}, {}]}'), ('{"d" : 01504193808, "t" : [{"a":"c"}, {}]}'), (null);
+SELECT get_json_object(a, '$.t'), get_json_object(a, '$.d') FROM test.test;
+DROP TABLE test.test;
