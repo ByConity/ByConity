fc1da46596d1829c048894d338e73fe19c87d0f9
ByConityBot <benyucong@bytedance.com>
2023-09-12 11:45:16 +0800

    Merge branch 'dao_cherry-pic' into 'cnch-ce-merge'
    fix(clickhousech@m-14715218): fix asan
    
    See merge request dp/ClickHouse!15411

--- src/DataStreams/OwningBlockInputStream.h
+++ src/DataStreams/OwningBlockInputStream.h
@@ -3,6 +3,8 @@
 #include <memory>
 
 #include <DataStreams/IBlockInputStream.h>
+#include <Poco/Logger.h>
+#include <common/logger_useful.h>
 
 namespace DB
 {
@@ -15,12 +17,19 @@ class OwningBlockInputStream : public IBlockInputStream
 {
 public:
     OwningBlockInputStream(const BlockInputStreamPtr & stream_, std::unique_ptr<OwnType> own_)
-        : stream{stream_}, own{std::move(own_)}
+        : own{std::move(own_)}, stream{stream_}
     {
         children.push_back(stream);
     }
 
     Block getHeader() const override { return children.at(0)->getHeader(); }
+
+    ~OwningBlockInputStream()
+    {
+        children.clear();
+        if (stream.use_count() > 1)
+            LOG_WARNING(&Poco::Logger::get("OwningBlockInputStream"), "The BlockInputStream might outlive the buffer!");
+    }
 
 private:
     Block readImpl() override { return stream->read(); }
@@ -28,8 +37,8 @@ private:
     String getName() const override { return "Owning"; }
 
 protected:
-    BlockInputStreamPtr stream;
     std::unique_ptr<OwnType> own;
+    BlockInputStreamPtr stream;
 };
 
 }
