f1cdeeadda404c9ccccfd0a1214bf8098fd56233
juppylm <31834016+juppylm@users.noreply.github.com>
2023-09-21 20:12:38 +0800

    fix a bug centos can not run

--- src/Common/SystemUtils.cpp
+++ src/Common/SystemUtils.cpp
@@ -18,6 +18,19 @@ __attribute__((constructor)) static void init_max_numa_node()
     std::stringstream buffer;
     buffer << fstream.rdbuf();
 
-    max_numa_node = std::stoul(buffer.str().substr(buffer.str().find('-') + 1));
+    try
+    {
+        max_numa_node = std::stoul(buffer.str().substr(buffer.str().find('-') + 1));
+    }
+    catch (const std::invalid_argument&) 
+    {
+        max_numa_node = 0;
+    }
+    catch (const std::out_of_range& e) 
+    {
+        max_numa_node = 0;
+    }
+
+    
 }
 }
