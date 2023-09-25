f4bf73762a5217478f47998e5aa46099d713e9b1
Dao <minh.dao@bytedance.com>
2023-09-25 11:41:36 +0800

    Merge branch 'ifnull_alias' into 'cnch-ce-merge'
    fix(clickhousech@m-14781343): add alias for ifNull
    
    See merge request dp/ClickHouse!15486

--- src/Functions/ifNull.cpp
+++ src/Functions/ifNull.cpp
@@ -91,6 +91,7 @@ private:
 void registerFunctionIfNull(FunctionFactory & factory)
 {
     factory.registerFunction<FunctionIfNull>(FunctionFactory::CaseInsensitive);
+    factory.registerAlias("nvl", FunctionIfNull::name, FunctionFactory::CaseInsensitive);
 }
 
 }
