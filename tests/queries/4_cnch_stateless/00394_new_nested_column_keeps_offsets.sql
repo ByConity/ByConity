bf2f05c47d273d83e1496a696109d08df859c72c
Dao <minh.dao@bytedance.com>
2023-12-13 11:43:43 +0800

    Merge branch 'cherry-pick-ab0963ae-2' into 'cnch-ce-merge'
    fix(clickhousech@m-16679531): [cp] cnch 2.0 fix nested column merge using prefetch reader
    
    See merge request dp/ClickHouse!17784

--- tests/queries/4_cnch_stateless/00394_new_nested_column_keeps_offsets.sql
+++ tests/queries/4_cnch_stateless/00394_new_nested_column_keeps_offsets.sql
@@ -1,3 +1,4 @@
+SET disable_optimize_final=0;
 
 DROP TABLE IF EXISTS alter_00394;
 CREATE TABLE alter_00394 (d Date, k UInt64, i32 Int32, n Nested(ui8 UInt8, s String)) 
@@ -11,6 +12,7 @@ ALTER TABLE alter_00394 ADD COLUMN `n.i8` Array(Int8) AFTER i32;
 SELECT `n.i8`, `n.ui8`, `n.s` FROM alter_00394 ORDER BY k;
 SELECT `n.i8` FROM alter_00394 ORDER BY k;
 SYSTEM START MERGES alter_00394;
-OPTIMIZE TABLE alter_00394 SETTINGS mutations_sync = 1;
+OPTIMIZE TABLE alter_00394 FINAL SETTINGS mutations_sync = 1;
+SELECT `n.i8`, `n.ui8`, `n.s` FROM alter_00394 ORDER BY k;
 SELECT `n.i8` FROM alter_00394 ORDER BY k;
 DROP TABLE IF EXISTS alter_00394;
