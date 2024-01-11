35c8df7a3689c8e5c91a13414407681810999963
blueskygzhz <476223335@qq.com>
2024-01-11 09:30:51 +0800

    fix sql syntax

--- tests/queries/4_cnch_stateless/50002_test_system_cnch_table.sql
+++ tests/queries/4_cnch_stateless/50002_test_system_cnch_table.sql
@@ -1,4 +1,4 @@
-SELECT '-- construct test db1 and tables --'
+SELECT '-- construct test db1 and tables --';
 CREATE DATABASE IF NOT EXISTS test_cnch_db1;
 CREATE TABLE test_cnch_db1.tb1(d Date, id UInt64, a String)
     ENGINE = CnchMergeTree()
@@ -7,7 +7,7 @@ CREATE TABLE test_cnch_db1.tb1(d Date, id UInt64, a String)
     ORDER BY `id`
     SAMPLE BY `id`;
 
-SELECT '-- construct test db2 and tables --'
+SELECT '-- construct test db2 and tables --';
 CREATE DATABASE IF NOT EXISTS test_cnch_db2;
 CREATE TABLE test_cnch_db2.tb1(d Date, id UInt64, a String)
     ENGINE = CnchMergeTree()
@@ -22,7 +22,7 @@ CREATE TABLE test_cnch_db2.tb2(d Date, id UInt64, a String)
     ORDER BY `id`
     SAMPLE BY `id`;
 
-SELECT '-- construct test db3 and tables --'
+SELECT '-- construct test db3 and tables --';
 CREATE DATABASE IF NOT EXISTS test_cnch_db3;
 CREATE TABLE test_cnch_db3.tb1(d Date, id UInt64, a String)
     ENGINE = CnchMergeTree()
@@ -43,16 +43,16 @@ CREATE TABLE test_cnch_db3.tb3(d Date, id UInt64, a String)
     ORDER BY `id`
     SAMPLE BY `id`;
 
-SELECT '-- test count --'
-SELECT COUNT(*) FROM system.cnch_tables where database like '%test_cnch_db%'
-SELECT '-- test single table query --'
-SELECT database, name FROM system.cnch_tables where database='test_cnch_db2' AND name='tb1'
-SELECT '-- test multiple tables or query --'
-SELECT database, name FROM system.cnch_tables where (database='test_cnch_db1' AND name='tb1') OR (database='test_cnch_db3' AND name='tb2') ORDER BY database
-SELECT '-- test db and db_table mixed query --'
-SELECT database, name FROM system.cnch_tables where (database like '%test_cnch_db%') OR (database='test_cnch_db3' AND name='tb3') ORDER BY database
+SELECT '-- test count --';
+SELECT COUNT(*) FROM system.cnch_tables where database like '%test_cnch_db%';
+SELECT '-- test single table query --';
+SELECT database, name FROM system.cnch_tables where database='test_cnch_db2' AND name='tb1';
+SELECT '-- test multiple tables or query --';
+SELECT database, name FROM system.cnch_tables where (database='test_cnch_db1' AND name='tb1') OR (database='test_cnch_db3' AND name='tb2') ORDER BY database;
+SELECT '-- test db and db_table mixed query --';
+SELECT database, name FROM system.cnch_tables where (database like '%test_cnch_db%') OR (database='test_cnch_db3' AND name='tb3') ORDER BY database;
 
-SELECT '-- clean --'
-DROP DATABASE test_cnch_db1
-DROP DATABASE test_cnch_db2
-DROP DATABASE test_cnch_db3
+SELECT '-- clean --';
+DROP DATABASE test_cnch_db1;
+DROP DATABASE test_cnch_db2;
+DROP DATABASE test_cnch_db3;
