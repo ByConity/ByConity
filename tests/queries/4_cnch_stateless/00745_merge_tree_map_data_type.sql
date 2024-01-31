56ef8a4d5b73b5a4b68c5451c58d007fb986dc24
Dao <minh.dao@bytedance.com>
2024-01-31 17:28:26 +0800

    Merge branch 'cnch_dev_fix_non_default_enum' into 'cnch-dev'
    fix(clickhousech@m-3000581850): cnch dev fix non-standard default enum
    
    See merge request dp/ClickHouse!18824
    # Conflicts:
    #	src/DataTypes/IDataType.cpp

--- tests/queries/4_cnch_stateless/00745_merge_tree_map_data_type.sql
+++ tests/queries/4_cnch_stateless/00745_merge_tree_map_data_type.sql
@@ -13,11 +13,12 @@ CREATE TABLE 00745_merge_tree_map_data_type (
     `date_map` Map(Date, Date) KV,
     `datetime_map` Map(DateTime('Asia/Shanghai'), DateTime('Asia/Shanghai')) KV,
     `uuid_map` Map(UUID, UUID) KV,
-    `enums_map` Map(Enum8('hello' = 0, 'world' = 1, 'foo' = -1), Int32) KV)
+    `enums_map` Map(Enum8('hello' = 0, 'world' = 1, 'foo' = -1), Enum8('hello' = 0, 'world' = 1, 'foo' = -1)) KV,
+    `new_enums_map` Map(String, Enum8('non-default' = 1)) KV)
 Engine=CnchMergeTree ORDER BY n settings min_bytes_for_wide_part = 0, enable_compact_map_data = 0;
 
-insert into 00745_merge_tree_map_data_type values(1, {'s1': 's1', 's2': 's2'}, {'s1': ['v1', 'v2'], 's2': ['v1', 'v2']}, {'s1': 's1', 's2': 's2'}, {1: 1, 2:2}, {'s1': 's1', 's2': 's2'}, {0.5: 0.5, 1.9: 1.9}, {'2022-06-14': '2022-06-14', '2022-06-15': '2022-06-15'}, {'2022-06-14 12:00:00': '2022-06-14 12:00:00', '2022-06-14 14:00:00': '2022-06-14 14:00:00'}, {'93fd2f47-4567-4eb8-9e7b-0e17f4c77837': '93fd2f47-4567-4eb8-9e7b-0e17f4c77837'}, {'world': 4});
-insert into 00745_merge_tree_map_data_type values(2, {'s3': 's3'}, {'s3': ['v3', 'v4']}, {'s3': 's3'}, {3: 3}, {'s3': 's3'}, {3.5: 3.5, 4.9: 4.9}, {'2022-06-16': '2022-06-16'}, {'2022-06-16 12:00:00': '2022-06-16 12:00:00'}, {'13fd2f47-4567-4eb8-9e7b-0e17f4c77837': '13fd2f47-4567-4eb8-9e7b-0e17f4c77837'}, {'hello': 1, 'foo': 2});
+insert into 00745_merge_tree_map_data_type values(1, {'s1': 's1', 's2': 's2'}, {'s1': ['v1', 'v2'], 's2': ['v1', 'v2']}, {'s1': 's1', 's2': 's2'}, {1: 1, 2:2}, {'s1': 's1', 's2': 's2'}, {0.5: 0.5, 1.9: 1.9}, {'2022-06-14': '2022-06-14', '2022-06-15': '2022-06-15'}, {'2022-06-14 12:00:00': '2022-06-14 12:00:00', '2022-06-14 14:00:00': '2022-06-14 14:00:00'}, {'93fd2f47-4567-4eb8-9e7b-0e17f4c77837': '93fd2f47-4567-4eb8-9e7b-0e17f4c77837'}, {'world': 'world'}, {});
+insert into 00745_merge_tree_map_data_type values(2, {'s3': 's3'}, {'s3': ['v3', 'v4']}, {'s3': 's3'}, {3: 3}, {'s3': 's3'}, {3.5: 3.5, 4.9: 4.9}, {'2022-06-16': '2022-06-16'}, {'2022-06-16 12:00:00': '2022-06-16 12:00:00'}, {'13fd2f47-4567-4eb8-9e7b-0e17f4c77837': '13fd2f47-4567-4eb8-9e7b-0e17f4c77837'}, {'hello': 'hello', 'foo': 'foo'}, {});
 
 select '';
 select 'select * :';
@@ -25,11 +26,11 @@ select * from 00745_merge_tree_map_data_type order by n;
 
 select '';
 select 'select [] :';
-select string_map['s1'], string_map['s10'], string_array_map['s2'], string_array_map['s10'], fixed_string_map['s1'::FixedString(2)], fixed_string_map['s8'::FixedString(2)], int_map[1], int_map[10], lowcardinality_map['s1'], lowcardinality_map['s10'], float_map[0.5], float_map[0.4], date_map['2022-06-14'], date_map['2021-06-14'], datetime_map['2022-06-14 12:00:00'], datetime_map['2021-06-14 12:00:00'], uuid_map['93fd2f47-4567-4eb8-9e7b-0e17f4c77837'], uuid_map['53fd2f47-4567-4eb8-9e7b-0e17f4c77837'], enums_map['hello'] from 00745_merge_tree_map_data_type order by n;
+select string_map['s1'], string_map['s10'], string_array_map['s2'], string_array_map['s10'], fixed_string_map['s1'::FixedString(2)], fixed_string_map['s8'::FixedString(2)], int_map[1], int_map[10], lowcardinality_map['s1'], lowcardinality_map['s10'], float_map[0.5], float_map[0.4], date_map['2022-06-14'], date_map['2021-06-14'], datetime_map['2022-06-14 12:00:00'], datetime_map['2021-06-14 12:00:00'], uuid_map['93fd2f47-4567-4eb8-9e7b-0e17f4c77837'], uuid_map['53fd2f47-4567-4eb8-9e7b-0e17f4c77837'], enums_map['hello'], new_enums_map['non-exists'] from 00745_merge_tree_map_data_type order by n;
 
 select '';
 select 'select {} :';
-select string_map{'s1'}, string_map{'s10'}, string_array_map{'s2'}, string_array_map{'s10'}, fixed_string_map{'s1'::FixedString(2)}, fixed_string_map{'s8'::FixedString(2)}, int_map{1}, int_map{10}, lowcardinality_map{'s1'}, lowcardinality_map{'s10'}, float_map{0.5}, float_map{0.4}, date_map{'2022-06-14'}, date_map{'2021-06-14'}, datetime_map{'2022-06-14 12:00:00'}, datetime_map{'2021-06-14 12:00:00'}, uuid_map{'93fd2f47-4567-4eb8-9e7b-0e17f4c77837'}, uuid_map{'53fd2f47-4567-4eb8-9e7b-0e17f4c77837'}, enums_map{'hello'} from 00745_merge_tree_map_data_type order by n;
+select string_map{'s1'}, string_map{'s10'}, string_array_map{'s2'}, string_array_map{'s10'}, fixed_string_map{'s1'::FixedString(2)}, fixed_string_map{'s8'::FixedString(2)}, int_map{1}, int_map{10}, lowcardinality_map{'s1'}, lowcardinality_map{'s10'}, float_map{0.5}, float_map{0.4}, date_map{'2022-06-14'}, date_map{'2021-06-14'}, datetime_map{'2022-06-14 12:00:00'}, datetime_map{'2021-06-14 12:00:00'}, uuid_map{'93fd2f47-4567-4eb8-9e7b-0e17f4c77837'}, uuid_map{'53fd2f47-4567-4eb8-9e7b-0e17f4c77837'}, enums_map{'hello'}, new_enums_map{'non-exists'} from 00745_merge_tree_map_data_type order by n;
 
 select '';
 select 'select mapKeys :';
