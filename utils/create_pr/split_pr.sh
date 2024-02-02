f5a328676b71b3ffbe6ba2afdcd51f7315fc1aa9
Dao <minh.dao@bytedance.com>
2024-02-02 20:05:42 +0800

    Merge branch 'dao_test_0' into 'cnch-dev'
    feat(clickhousech@m-12000): toy message0
    
    See merge request dp/ClickHouse!1

---
+++ utils/create_pr/split_pr.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    BRANCH=$(git branch --show-current)
+else
+    BRANCH=$1
+fi
+RAW_TITLE=$(git log refs/heads/$BRANCH^! | tail -n +4 | grep -v "Merge branch" |sed '/^[[:space:]]*$/d' | grep -v "See merge request dp")
+TITLE=$(echo $RAW_TITLE| sed -E -e 's/^(feat|fix|build|ci|docs|perf|refactor|style|test)(\((clickhousech|optimizer)+@m-[1-9][0-9]+\)):+(.+)$/\1:\4/')
+
+echo "create pr with title: ${TITLE}"
+gh pr create --base master --head $BRANCH --title "$TITLE" --body "$TITLE"
