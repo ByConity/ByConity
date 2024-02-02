6b0540475a1f112f4fd2e608102ad456f01a7dba
Dao <minh.dao@bytedance.com>
2024-02-02 11:34:09 +0800

    Merge branch 'jiacheng.sun-fix-mysql-from-base64' into 'cnch-dev'
    fix(clickhousech@m-3000280004): fix mysql FROM_BASE64
    
    See merge request dp/ClickHouse!18506

--- tests/queries/4_cnch_stateless/01659_test_base64Decode_mysql_compatibility.sql
+++ tests/queries/4_cnch_stateless/01659_test_base64Decode_mysql_compatibility.sql
@@ -1,1 +1,21 @@
 SELECT FROM_BASE64('dGVzdCBzdHJpbmc=');
+SELECT FROM_BASE64('???'); --{serverError 117}
+
+CREATE TABLE 01659_test_base64Decode_mysql_compatibility (base64_str String) ENGINE = CnchMergeTree ORDER BY tuple();
+INSERT INTO 01659_test_base64Decode_mysql_compatibility VALUES ('dGVzdCBzdHJpbmc='), ('???'), ('dGVzdCBzdHJpbmc='), ('???');
+
+SELECT FROM_BASE64(base64_str) FROM 01659_test_base64Decode_mysql_compatibility; --{serverError 117}
+
+DROP TABLE 01659_test_base64Decode_mysql_compatibility;
+
+
+SET dialect_type='MYSQL';
+SELECT FROM_BASE64('dGVzdCBzdHJpbmc=');
+SELECT FROM_BASE64('???');
+
+CREATE TABLE 01659_test_base64Decode_mysql_compatibility (base64_str String) ENGINE = CnchMergeTree ORDER BY tuple();
+INSERT INTO 01659_test_base64Decode_mysql_compatibility VALUES ('dGVzdCBzdHJpbmc='), ('???'), ('dGVzdCBzdHJpbmc='), ('???');
+
+SELECT FROM_BASE64(base64_str) FROM 01659_test_base64Decode_mysql_compatibility;
+
+DROP TABLE 01659_test_base64Decode_mysql_compatibility;
