2232b5aca601fbe093c44766159252b1daa4ea00
Dao <minh.dao@bytedance.com>
2024-02-05 11:41:10 +0800

    Merge branch 'dao_test_4' into 'cnch-dev'
    feat(clickhousech@m-12004): toy message4
    
    See merge request dp/ClickHouse!4

--- utils/create_pr/split_pr.sh
+++ utils/create_pr/split_pr.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
+REMOTE="byconity"
+
 if [ -z "$1" ]; then
     BRANCH=$(git branch --show-current)
 else
     BRANCH=$1
 fi
 
-COMMITS=$(git log --no-merges --format=%H --extended-regexp --grep='^(feat|fix|build|ci|docs|perf|refactor|style|test)(\((clickhousech|optimizer)+@m-[1-9][0-9]+\)):+(.+)$' cherry-pick ^master)
+COMMITS=$(git log --no-merges --format=%H --extended-regexp --grep='^(feat|fix|build|ci|docs|perf|refactor|style|test)(\((clickhousech|optimizer)+@m-[1-9][0-9]+\)):+(.+)$' $BRANCH~ ^master)
 index=1
-for COMMIT in $COMMITS:
+for COMMIT in $COMMITS;
 do
     echo "create pr for commit $COMMIT"
-    git co -b BRANCH_sub${index} COMMIT
+    SUB_BRANCH=${BRANCH}_sub${index}
+    git co -b ${SUB_BRANCH} $COMMIT
     ((index++)) # increase the index
-    RAW_TITLE=$(git log refs/heads/$BRANCH^! | tail -n +4 | grep -v "Merge branch" |sed '/^[[:space:]]*$/d' | grep -v "See merge request dp")
+    git push -u -f ${REMOTE} ${SUB_BRANCH}
+    RAW_TITLE=$(git log refs/heads/$SUB_BRANCH^! | tail -n +4 | grep -v "Merge branch" |sed '/^[[:space:]]*$/d' | grep -v "See merge request dp")
     TITLE=$(echo $RAW_TITLE| sed -E -e 's/^(feat|fix|build|ci|docs|perf|refactor|style|test)(\((clickhousech|optimizer)+@m-[1-9][0-9]+\)):+(.+)$/\1:\4/')
 
     echo "create pr with title: ${TITLE}"
-    gh pr create --base master --head $BRANCH --title "$TITLE" --body "$TITLE"
+    gh pr create --base master --head $SUB_BRANCH --title "$TITLE" --body "$TITLE"
 done
 
