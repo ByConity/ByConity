8281d0ff2a4a51bff6bab6fcf476a4efc9b5e94c
Dao <minh.dao@bytedance.com>
2024-02-05 11:10:40 +0800

    Merge branch 'dao_test_1' into 'cnch-dev'
    feat(clickhousech@m-12001): toy message1
    
    See merge request dp/ClickHouse!2

--- utils/create_pr/split_pr.sh
+++ utils/create_pr/split_pr.sh
@@ -5,8 +5,16 @@ if [ -z "$1" ]; then
 else
     BRANCH=$1
 fi
-RAW_TITLE=$(git log refs/heads/$BRANCH^! | tail -n +4 | grep -v "Merge branch" |sed '/^[[:space:]]*$/d' | grep -v "See merge request dp")
-TITLE=$(echo $RAW_TITLE| sed -E -e 's/^(feat|fix|build|ci|docs|perf|refactor|style|test)(\((clickhousech|optimizer)+@m-[1-9][0-9]+\)):+(.+)$/\1:\4/')
 
-echo "create pr with title: ${TITLE}"
-gh pr create --base master --head $BRANCH --title "$TITLE" --body "$TITLE"
+COMMITS=$(git log --no-merges --format=%H --extended-regexp --grep='^(feat|fix|build|ci|docs|perf|refactor|style|test)(\((clickhousech|optimizer)+@m-[1-9][0-9]+\)):+(.+)$' cherry-pick ^master)
+for COMMIT in $COMMITS:
+do
+    echo "create pr for commit $COMMIT"
+    git co -b 
+done
+
+#RAW_TITLE=$(git log refs/heads/$BRANCH^! | tail -n +4 | grep -v "Merge branch" |sed '/^[[:space:]]*$/d' | grep -v "See merge request dp")
+#TITLE=$(echo $RAW_TITLE| sed -E -e 's/^(feat|fix|build|ci|docs|perf|refactor|style|test)(\((clickhousech|optimizer)+@m-[1-9][0-9]+\)):+(.+)$/\1:\4/')
+#
+#echo "create pr with title: ${TITLE}"
+#gh pr create --base master --head $BRANCH --title "$TITLE" --body "$TITLE"
