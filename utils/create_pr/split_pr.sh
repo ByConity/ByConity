3028dc539ddf27a9e07fad8d599c4ac464299edb
Dao <minh.dao@bytedance.com>
2024-02-07 15:30:18 +0800

    ci: minor fix ci script

--- utils/create_pr/split_pr.sh
+++ utils/create_pr/split_pr.sh
@@ -8,8 +8,9 @@ else
     BRANCH=$1
 fi
 
+
 ABBREVIATED_TOP_COMMIT_HASH=$(git log refs/heads/${BRANCH}^! --format=%h)
-COMMITS=$(git log --no-merges --format=%H --extended-regexp --grep='^(feat|fix|build|ci|docs|perf|refactor|style|test)(\((clickhousech|optimizer)+@m-[1-9][0-9]+\)):+(.+)$' $BRANCH~ ^master)
+COMMITS=$(git log --no-merges --format=%H --extended-regexp --grep='^(feat|fix|build|ci|docs|perf|refactor|style|test)(\((clickhousech|optimizer)+@m-[1-9][0-9]+\))*:+(.+)$' $BRANCH~ ^master)
 index=1
 
 for COMMIT in $COMMITS;
@@ -20,10 +21,10 @@ do
     ((index++)) # increase the index
     git push -u -f ${REMOTE} ${SUB_BRANCH}
     RAW_TITLE=$(git log refs/heads/$SUB_BRANCH^! | tail -n +4 | grep -v "Merge branch" |sed '/^[[:space:]]*$/d' | grep -v "See merge request dp"| grep -v "#")
-    TITLE=$(echo $RAW_TITLE| sed -E -e 's/^(feat|fix|build|ci|docs|perf|refactor|style|test)(\((clickhousech|optimizer)+@m-[1-9][0-9]+\)):+(.+)$/\1:\4/')
+    TITLE=$(echo $RAW_TITLE| sed -E -e 's/^(feat|fix|build|ci|docs|perf|refactor|style|test)(\((clickhousech|optimizer)+@m-[1-9][0-9]+\))*:+(.+)$/\1:\4/')
 
     echo "create pr with title: ${TITLE}"
-    gh pr create --base master --head $SUB_BRANCH --title "$TITLE" --body "$TITLE" && git br -D $SUB_BRANCH
+    gh pr create --base master --head $SUB_BRANCH --title "$TITLE" --body "$TITLE" && 
 done
 
 
