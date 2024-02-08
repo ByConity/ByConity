3836d11c78fb7444a0640c95b641a1c739b12c86
Dao <minh.dao@bytedance.com>
2024-02-08 10:35:54 +0800

    Merge branch 'zema/cnch-dev-fixCIOptimizeQuery' into 'cnch-dev'
    fix(clickhousech@m-3001029266): fix 10120_optimize_query
    
    See merge request dp/ClickHouse!19050

--- tests/queries/4_cnch_stateless/10120_optimize_query.sql
+++ tests/queries/4_cnch_stateless/10120_optimize_query.sql
@@ -2,6 +2,8 @@ SET mutations_sync = 1;
 
 DROP TABLE IF EXISTS test_optimize_10120;
 CREATE TABLE test_optimize_10120(x Int32, y String) Engine = CnchMergeTree ORDER BY x;
+SYSTEM START MERGES test_optimize_10120;
+SYSTEM STOP MERGES test_optimize_10120;
 
 INSERT INTO test_optimize_10120 VALUES(1, '1');
 INSERT INTO test_optimize_10120 VALUES(2, '2');
@@ -9,7 +11,6 @@ INSERT INTO test_optimize_10120 VALUES(3, '3');
 INSERT INTO test_optimize_10120 VALUES(4, '4');
 INSERT INTO test_optimize_10120 VALUES(5, '5');
 
-SYSTEM START MERGES test_optimize_10120;
 OPTIMIZE TABLE test_optimize_10120 PARTITION ID 'all';
 
 SELECT * FROM test_optimize_10120;
