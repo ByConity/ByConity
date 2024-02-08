748a1e1ba7d8215d0f2199f14890956092ef34e3
Dao <minh.dao@bytedance.com>
2024-02-08 14:15:27 +0800

    Merge branch 'cherry-pick-853e8717-2' into 'cnch-dev'
    fix(clickhousech@m-15221885): fix quantileTDigestWithSSize
    
    See merge request dp/ClickHouse!19264

--- tests/queries/4_cnch_stateless/00273_quantiles.sql
+++ tests/queries/4_cnch_stateless/00273_quantiles.sql
@@ -10,8 +10,6 @@ SELECT quantilesDeterministic(0, 0.001, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6
 
 SELECT round(1000000 / (number + 1)) AS k, count() AS c, quantilesDeterministic(0.1, 0.5, 0.9)(number, intHash64(number)) AS q1, quantilesExact(0.1, 0.5, 0.9)(number) AS q2 FROM (SELECT number FROM system.numbers LIMIT 1000000) GROUP BY k ORDER BY k;
 
--- just for compatible, something wrong under `enable_optimizer=1, enable_optimizer_fallback=0`, temporarily skip it first
-SET enable_optimizer = 0;
 SELECT quantileTDigestWithSSize([0.2], 1028)(x) FROM (SELECT number AS x FROM system.numbers LIMIT 1001);
 SELECT quantilesTDigestWithSSize([0, 0.001, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99, 0.999, 1])(x) FROM (SELECT number AS x FROM system.numbers LIMIT 1001);
 SELECT quantileTDigestWithSSizeWeighted([0.8])(x, x%10) FROM (SELECT number AS x FROM system.numbers LIMIT 1001);
