1f018ce8cf2afef802ccf5255ddf6a945c36b94e
blueskygzhz <476223335@qq.com>
2024-02-18 14:20:37 +0800

    add error case

--- tests/queries/4_cnch_stateless/10030_str_to_arraystring_check.sql
+++ tests/queries/4_cnch_stateless/10030_str_to_arraystring_check.sql
@@ -1,6 +1,6 @@
 SELECT '-- construct test db1 and tables --';
 CREATE DATABASE IF NOT EXISTS test_cnch_ddl_check_db1;
-CREATE TABLE test_cnch_ddl_check_db1.tb1(d Date, id UInt64, a String)
+CREATE TABLE IF NOT EXISTS test_cnch_ddl_check_db1.tb1(d Date, id UInt64, a String)
     ENGINE = CnchMergeTree()
     PARTITION BY `d`
     PRIMARY KEY `id`
@@ -8,7 +8,7 @@ CREATE TABLE test_cnch_ddl_check_db1.tb1(d Date, id UInt64, a String)
     SAMPLE BY `id`;
 
 SELECT '-- test modify String to Array(String) --';
-alter table test_cnch_ddl_check_db1.tb1 modify column a Array(String);
+alter table test_cnch_ddl_check_db1.tb1 modify column a Array(String); --{serverError 53} CAST AS Array can only be performed between same-dimensional Array or String types
 
 SELECT '-- clean --';
 DROP DATABASE test_cnch_ddl_check_db1;
