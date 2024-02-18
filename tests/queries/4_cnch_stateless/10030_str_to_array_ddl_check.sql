c73ffa15ed55d9d01e7f00808e7266092ecbc379
blueskygzhz <476223335@qq.com>
2024-02-18 16:20:36 +0800

    add more test cases

--- tests/queries/4_cnch_stateless/10030_str_to_array_ddl_check.sql
+++ tests/queries/4_cnch_stateless/10030_str_to_array_ddl_check.sql
@@ -1,6 +1,6 @@
 SELECT '-- construct test db1 and tables --';
 CREATE DATABASE IF NOT EXISTS test_cnch_ddl_check_db1;
-CREATE TABLE IF NOT EXISTS test_cnch_ddl_check_db1.tb1(d Date, id UInt64, a String)
+CREATE TABLE IF NOT EXISTS test_cnch_ddl_check_db1.tb1(d Date, id UInt64, a String, c Int64)
     ENGINE = CnchMergeTree()
     PARTITION BY `d`
     PRIMARY KEY `id`
@@ -9,6 +9,7 @@ CREATE TABLE IF NOT EXISTS test_cnch_ddl_check_db1.tb1(d Date, id UInt64, a Stri
 
 SELECT '-- test modify String to Array(String) --';
 alter table test_cnch_ddl_check_db1.tb1 modify column a Array(String); --{serverError 53} CAST AS Array can only be performed between same-dimensional Array or String types
+alter table test_cnch_ddl_check_db1.tb1 modify column c UInt64;
 
 SELECT '-- clean --';
 DROP DATABASE test_cnch_ddl_check_db1;
