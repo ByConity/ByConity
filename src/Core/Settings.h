1514345dae4a2ef8a10ff6088aa376d7ca4961cc
Dao <minh.dao@bytedance.com>
2024-03-13 17:30:03 +0800

    Merge branch 'pick_read_json_as_string' into 'cnch-dev'
    fix(clickhousech@m-3011463128): pick input_format_json_read_objects_as_strings setting for compatibility
    
    See merge request dp/ClickHouse!19799
    # Conflicts:
    #	src/Core/Settings.h

--- src/Core/Settings.h
+++ src/Core/Settings.h
@@ -1931,22 +1931,10 @@ enum PreloadLevelSettings : UInt64
     M(Bool, output_format_csv_crlf_end_of_line, false, "If it is set true, end of line in CSV format will be \\r\\n instead of \\n.", 0) \
     M(Bool, input_format_csv_unquoted_null_literal_as_null, false, "Consider unquoted NULL literal as \\N", 0) \
     M(Bool, input_format_csv_enum_as_number, false, "Treat inserted enum values in CSV formats as enum indices \\N", 0) \
-    M(Bool, \
-      input_format_csv_arrays_as_nested_csv, \
-      false, \
-      R"(When reading Array from CSV, expect that its elements were serialized in nested CSV and then put into string. Example: "[""Hello"", ""world"", ""42"""" TV""]". Braces around array can be omitted.)", \
-      0) \
-    M(Bool, \
-      input_format_skip_unknown_fields, \
-      false, \
-      "Skip columns with unknown names from input data (it works for JSONEachRow, CSVWithNames, TSVWithNames and TSKV formats).", \
-      0) \
-    M(Bool, \
-      input_format_with_names_use_header, \
-      true, \
-      "For TSVWithNames and CSVWithNames input formats this controls whether format parser is to assume that column data appear in the " \
-      "input exactly as they are specified in the header.", \
-      0) \
+    M(Bool, input_format_csv_arrays_as_nested_csv, false, R"(When reading Array from CSV, expect that its elements were serialized in nested CSV and then put into string. Example: "[""Hello"", ""world"", ""42"""" TV""]". Braces around array can be omitted.)", 0) \
+    M(Bool, input_format_json_read_objects_as_strings, false, "Allow to parse JSON objects as strings in JSON input formats; Do NOT set it true by default as it breaks the logic of parsing JSON", 0) \
+    M(Bool, input_format_skip_unknown_fields, false, "Skip columns with unknown names from input data (it works for JSONEachRow, CSVWithNames, TSVWithNames and TSKV formats).", 0) \
+    M(Bool, input_format_with_names_use_header, true, "For TSVWithNames and CSVWithNames input formats this controls whether format parser is to assume that column data appear in the input exactly as they are specified in the header.", 0) \
     M(Bool, input_format_import_nested_json, false, "Map nested JSON data to nested tables (it works for JSONEachRow format).", 0) \
     M(Bool, \
       input_format_defaults_for_omitted_fields, \
---
+++ tests/queries/4_cnch_stateless/11016_parse_object_as_string.sql
@@ -0,0 +1,14 @@
+use test;
+DROP TABLE IF EXISTS test_parse_json_object_as_strings;
+
+CREATE TABLE test_parse_json_object_as_strings (obj String, params Array(String)) ENGINE = CnchMergeTree() ORDER BY tuple();
+
+SELECT 'Setting input_format_json_read_objects_as_strings should be disabled by default';
+SELECT value FROM system.settings WHERE name = 'input_format_json_read_objects_as_strings';
+
+SELECT 'INSERT with setting input_format_json_read_objects_as_strings = 1';
+INSERT INTO test_parse_json_object_as_strings FORMAT JSONEachRow SETTINGS input_format_json_read_objects_as_strings = 1 {"obj":{"name":"durian"}, "params":[{"property0":"value0"},{"property1":"value1"}]};
+
+SELECT * FROM test_parse_json_object_as_strings FORMAT Vertical;
+
+DROP TABLE test_parse_json_object_as_strings;
