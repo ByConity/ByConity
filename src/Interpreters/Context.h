04c3905e10134f3fc8a06ee1a22b6a569e3b2d91
Dao <minh.dao@bytedance.com>
2024-04-02 14:28:13 +0800

    Merge branch 'mysql-rbac-user' into 'cnch-dev'
    feat(clickhousech@m-3346761604): Separate user name for mysql protocol
    
    See merge request dp/ClickHouse!20253
    # Conflicts:
    #	src/Interpreters/Context.cpp

--- src/Interpreters/Context.h
+++ src/Interpreters/Context.h
@@ -729,6 +729,7 @@ public:
     void setUsersConfig(const ConfigurationPtr & config);
     ConfigurationPtr getUsersConfig();
 
+    String formatUserName(const String & name);
     /// Sets the current user, checks the credentials and that the specified host is allowed.
     /// Must be called before getClientInfo() can be called.
     void setUser(const Credentials & credentials, const Poco::Net::SocketAddress & address);
