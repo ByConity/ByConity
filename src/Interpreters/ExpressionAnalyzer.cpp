4b29391c1a05717a116dd7b4be6c11ad63ca8322
jay <jay@bytedance.com>
2024-04-03 14:50:02 +0800

    Merge branch 'grace-hash-join-rehash-fix-cnch' into 'cnch-dev'
    fix(clickhousech@m-3001140632): fix bug when grace hash join rehash
    
    See merge request dp/ClickHouse!19661

--- src/Interpreters/ExpressionAnalyzer.cpp
+++ src/Interpreters/ExpressionAnalyzer.cpp
@@ -1312,7 +1312,10 @@ static std::shared_ptr<IJoin> makeJoin(std::shared_ptr<TableJoin> analyzed_join,
     else if (analyzed_join->forceNestedLoopJoin())
         return std::make_shared<NestedLoopJoin>(analyzed_join, r_sample_block, context);
     else if (analyzed_join->forceGraceHashLoopJoin())
-        return std::make_shared<GraceHashJoin>(context, analyzed_join, l_sample_block, r_sample_block, context->getTempDataOnDisk(), 0);
+    {
+        auto parallel = (context->getSettingsRef().grace_hash_join_left_side_parallel != 0 ? context->getSettingsRef().grace_hash_join_left_side_parallel: context->getSettings().max_threads);
+        return std::make_shared<GraceHashJoin>(context, analyzed_join, l_sample_block, r_sample_block, context->getTempDataOnDisk(), parallel);
+    }
     return std::make_shared<JoinSwitcher>(analyzed_join, r_sample_block);
 }
 
