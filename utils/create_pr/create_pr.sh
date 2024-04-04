1b0f18dee874f3528b189750295e7c0e79f5e7b9
Dao <minh.dao@bytedance.com>
2024-04-04 14:43:21 +0800

    chore: fix utils script

--- utils/create_pr/create_pr.sh
+++ utils/create_pr/create_pr.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 source $(dirname "$0")/proc_title.sh
 
-if [ -z "$1" ]; then
+if [ -z "$2" ]; then
     BRANCH=$(git branch --show-current)
 else
-    BRANCH=$1
+    BRANCH=$2
 fi
+
+if [ -z "$1" ]; then
+    TARGET_BRANCH=master
+else
+    TARGET_BRANCH=$2
+fi
+
 
 GIT_LOG=$(git log refs/heads/$BRANCH^! | tail -n +4)
 TITLE=$(process_header "$GIT_LOG")
 
 echo "create pr with title: ${TITLE} for branch ${BRANCH}"
-gh pr create --base master --head $BRANCH --title "$TITLE" --body "$TITLE"
+gh pr create --base ${TARGET_BRANCH} --head $BRANCH --title "$TITLE" --body "$TITLE"
 
