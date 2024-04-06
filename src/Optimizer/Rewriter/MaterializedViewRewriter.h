f91576b3e80f2be20a6c22307428b4cc9e941e78
jay <jay@bytedance.com>
2024-04-06 13:04:25 +0800

    Merge branch 'fix/add_settings_enable_view_based_query_rewrite' into 'cnch-dev'
    fix(optimizer@m-3057453562): add settings enable_view_based_query_rewrite
    
    See merge request dp/ClickHouse!20186

--- src/Optimizer/Rewriter/MaterializedViewRewriter.h
+++ src/Optimizer/Rewriter/MaterializedViewRewriter.h
@@ -38,7 +38,10 @@ public:
 
 private:
     void rewrite(QueryPlan & plan, ContextMutablePtr context) const override;
-    bool isEnabled(ContextMutablePtr context) const override { return context->getSettingsRef().enable_materialized_view_rewrite; }
+    bool isEnabled(ContextMutablePtr context) const override
+    {
+        return context->getSettingsRef().enable_materialized_view_rewrite || context->getSettingsRef().enable_view_based_query_rewrite;
+    }
     static LinkedHashMap<MaterializedViewStructurePtr, PartitionCheckResult>
     getRelatedMaterializedViews(QueryPlan & plan, ContextMutablePtr context);
 };
