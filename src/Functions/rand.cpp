e2de5b949233e8da3cf250fb6ee32a4e20f161c8
Dao <minh.dao@bytedance.com>
2024-04-08 16:02:14 +0800

    Merge branch 'jiacheng.sun-fix-mysql-rand' into 'cnch-dev'
    fix(clickhousech@m-14362905): Redirect rand to randCanonical in mysql mode
    
    See merge request dp/ClickHouse!20696

--- src/Functions/rand.cpp
+++ src/Functions/rand.cpp
@@ -1,5 +1,6 @@
 #include <Functions/FunctionFactory.h>
 #include <Functions/FunctionsRandom.h>
+#include <Functions/canonicalRand.h>
 
 namespace DB
 {
@@ -7,7 +8,20 @@ namespace
 {
 
 struct NameRand { static constexpr auto name = "rand"; };
-using FunctionRand = FunctionRandom<UInt32, NameRand>;
+struct FunctionRand : public FunctionRandom<UInt32, NameRand>
+{
+    explicit FunctionRand(ContextPtr context)
+        : FunctionRandom(context)
+    {}
+
+    static FunctionPtr create(ContextPtr context)
+    {
+        if (context->getSettings().dialect_type == DialectType::MYSQL)
+            return FunctionCanonicalRand::create(context);
+        else
+            return std::make_shared<FunctionRand>(context);
+    }
+};
 
 }
 
