0de28a7fbffab4cf2e2f5e8cb58ebab05296d813
fanqi1909 <jay@bytedance.com>
2024-05-13 17:52:12 +0800

    Merge branch '2.1_or_depth_prewhere' into 'cnch-2.1'
    feat(clickhousech@m-3000207505): limit the size of or expressions to pushdown to prewhere
    
    See merge request dp/ClickHouse!21189

--- src/Core/Settings.h
+++ src/Core/Settings.h
@@ -1539,6 +1539,7 @@ enum PreloadLevelSettings : UInt64
     M(Bool, enable_subcolumn_optimization_through_union, true, "Whether enable sub column optimization through set operation.", 0) \
     M(Bool, enable_buffer_for_deadlock_cte, true, "Whether to buffer data for deadlock cte", 0) \
     M(UInt64, statistics_collect_debug_level, 0, "Debug level for statistics collector", 0) \
+    M(UInt64, max_prewhere_or_expression_size, 0, "Max depth of condition which can push down to prewhere", 0) \
     M(Bool, enable_add_exchange, true, "Whether to enable AddExchange rule", 0) \
     M(Bool, enable_bitmap_index_splitter, true, "Whether to enable BitMapIndexSplitter", 0) \
     M(Bool, enable_column_pruning, true, "Whether to enable ColumnPruning", 0) \
