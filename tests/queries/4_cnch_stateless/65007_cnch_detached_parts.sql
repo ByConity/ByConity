6d5ffc6888a11b7dfed2edb84ea89c113cdff09c
skyoct <skyoct@163.com>
2024-06-27 10:03:14 +0000

    fix tests

--- tests/queries/4_cnch_stateless/65007_cnch_detached_parts.sql
+++ tests/queries/4_cnch_stateless/65007_cnch_detached_parts.sql
@@ -3,6 +3,7 @@ CREATE TABLE lc_01 (date String,b LowCardinality(String)) ENGINE = CnchMergeTree
 INSERT INTO lc_01 VALUES ('2024-06-18', 'test'), ('2024-06-18', 'test_01');
 SELECT COUNT() FROM lc_01;
 ALTER TABLE lc_01 DELETE WHERE b = 'test_01';
+OPTIMIZE TABLE lc_01;
 SELECT COUNT() FROM lc_01;
 ALTER TABLE  lc_01 DETACH PARTITION  '2024-06-18';
 SELECT count() FROM  system.cnch_detached_parts WHERE database = currentDatabase() and table = 'lc_01';
