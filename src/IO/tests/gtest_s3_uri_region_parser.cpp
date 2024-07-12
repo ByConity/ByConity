19c8c32553cffe21e53ea961a12fc0043fd8c9cc
fredwang <fredwang@bytedance.com>
2024-07-12 05:48:47 +0000

    Merge branch 'jiashuo_fix-tos-parser' into 'cnch-dev'
    fix(clickhousech@m-4050338290): fix cnch s3 region parser failed
    
    See merge request dp/ClickHouse!21708

--- src/IO/tests/gtest_s3_uri_region_parser.cpp
+++ src/IO/tests/gtest_s3_uri_region_parser.cpp
@@ -13,18 +13,18 @@ TEST(S3Common, UriRegionParse)
     S3::URI s3_http_virtual_host("https://bucket-name.s3.region-code.amazonaws.com/key-name", true);
     ASSERT_EQ(s3_http_virtual_host.region, "region-code");
 
-    S3::URI tos_http("http://tos-region-code.ivolces.com/bucket-name/key-name");
-    ASSERT_EQ(s3_http.region, "region-code");
-    S3::URI tos_http_virtual_host("http://bucket-name.tos-region-code.ivolces.com/key-name");
-    ASSERT_EQ(s3_http.region, "region-code");
-    S3::URI tos_http_s3("http://tos-s3-region-code.ivolces.com/bucket-name/key-name");
-    ASSERT_EQ(s3_http.region, "region-code");
-    S3::URI tos_http_s3_virtual_host("http://bucket-name.tos-s3-region-code.ivolces.com/key-name");
-    ASSERT_EQ(s3_http.region, "region-code");
+    S3::URI tos_http("http://tos-region-code.ivolces.com/bucket-name/key-name", true);
+    ASSERT_EQ(tos_http.region, "region-code");
+    S3::URI tos_http_virtual_host("http://bucket-name.tos-region-code.ivolces.com/key-name", true);
+    ASSERT_EQ(tos_http_virtual_host.region, "region-code");
+    S3::URI tos_http_s3("http://tos-s3-region-code.ivolces.com/bucket-name/key-name", true);
+    ASSERT_EQ(tos_http_s3.region, "region-code");
+    S3::URI tos_http_s3_virtual_host("http://bucket-name.tos-s3-region-code.ivolces.com/key-name", true);
+    ASSERT_EQ(tos_http_s3_virtual_host.region, "region-code");
 
-    S3::URI cos_http("http://cos.region-code.myqcloud.com/bucket-name/key-name");
-    ASSERT_EQ(s3_http.region, "region-code");
-    S3::URI cos_http_virtual_host("http://bucket-name.cos.region-code.myqcloud.com/key-name");
-    ASSERT_EQ(s3_http.region, "region-code");
+    S3::URI cos_http("http://cos.region-code.myqcloud.com/bucket-name/key-name", true);
+    ASSERT_EQ(cos_http.region, "region-code");
+    S3::URI cos_http_virtual_host("http://bucket-name.cos.region-code.myqcloud.com/key-name", true);
+    ASSERT_EQ(cos_http_virtual_host.region, "region-code");
 }
 
