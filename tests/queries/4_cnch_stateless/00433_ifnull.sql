51fca75a1090d6c6b9dfff4e89cb4bc99b918e79
fredwang <fredwang@bytedance.com>
2024-07-29 09:12:24 +0000

    Merge 'cherry-pick-6e60cb0c' into 'cnch-dev'
    fix(clickhousech@m-4086549751): Fix IfNull and Case Type Mismatch dev
    
    See merge request: #22210

--- tests/queries/4_cnch_stateless/00433_ifnull.sql
+++ tests/queries/4_cnch_stateless/00433_ifnull.sql
@@ -19,3 +19,21 @@ SELECT ifNull(1, NULL) AS res, toTypeName(res);
 SELECT ifNull(NULL, NULL) AS res, toTypeName(res);
 
 SELECT IFNULL(NULLIF(toString(number), '1'), NULLIF(toString(-number), '-3')) AS res, toTypeName(res) FROM system.numbers LIMIT 5;
+
+set dialect_type='MYSQL';
+drop table if exists 00433_t1;
+drop table if exists 00433_t2;
+
+SELECT ifNull(toNullable('x'), toNullable(1)) AS res, toTypeName(res);
+SELECT NULLIF(5,5) IS NULL, NULLIF(5,'5') IS NOT NULL;
+create table 00433_t1 (a int, b int);
+insert into 00433_t1 values(null, null), (0, null), (1, null), (null, 0), (null, 1), (0, 0), (0, 1), (1, 0), (1, 1);
+select ifnull(a, 'N') as A, ifnull(b, 'N') as B, ifnull(not a, 'N') as nA, ifnull(not b, 'N') as nB, ifnull(a and b, 'N') as AB, ifnull(not (a and b), 'N') as `n(AB)`, ifnull((not a or not b), 'N') as nAonB, ifnull(a or b, 'N') as AoB, ifnull(not(a or b), 'N') as `n(AoB)`, ifnull(not a and not b, 'N') as nAnB from 00433_t1;
+
+select CASE 'b' when 'a' then 1 when 'B' then 2 WHEN 'b' then 'ok' END;
+
+create table 00433_t2 (num  double(12,2));
+insert into 00433_t2 values (144.54);
+select sum(if(num is null,0.00,num)) from 00433_t2;
+drop table if exists 00433_t1;
+drop table if exists 00433_t2;
