09ec09d626054da99fa81ff6094a459e648199a4
fredwang <fredwang@bytedance.com>
2024-08-02 08:46:50 +0000

    Merge 'fix/fix_mark_distinct_optimization_2.2' into 'cnch-2.2'
    fix(optimizer@m-4674753509): MultipleDistinctAggregationToMarkDistinct supports count distinct with multi columns cnch-2.2
    
    See merge request: !23102

--- src/Optimizer/Rule/Rewrite/MultipleDistinctAggregationToMarkDistinct.cpp
+++ src/Optimizer/Rule/Rewrite/MultipleDistinctAggregationToMarkDistinct.cpp
@@ -1,6 +1,13 @@
+#include <memory>
 #include <AggregateFunctions/AggregateFunctionFactory.h>
+#include <Core/NameToType.h>
+#include <DataTypes/DataTypesNumber.h>
+#include <DataTypes/Serializations/ISerialization.h>
+#include <Optimizer/PredicateUtils.h>
 #include <Optimizer/Rule/Patterns.h>
 #include <Optimizer/Rule/Rewrite/MultipleDistinctAggregationToMarkDistinct.h>
+#include <Parsers/ASTFunction.h>
+#include <Parsers/ASTIdentifier.h>
 #include <QueryPlan/AggregatingStep.h>
 #include <QueryPlan/MarkDistinctStep.h>
 
@@ -75,6 +82,8 @@ TransformResult MultipleDistinctAggregationToMarkDistinct::transformImpl(PlanNod
     AggregateDescriptions new_agg_descs;
     PlanNodePtr child = node->getChildren()[0];
 
+    Assignments new_argument_assignments;
+
     for (const auto & agg_desc : agg_descs)
     {
         if (distinct_func.contains(Poco::toLower(agg_desc.function->getName())) && agg_desc.mask_column.empty())
@@ -114,18 +123,40 @@ TransformResult MultipleDistinctAggregationToMarkDistinct::transformImpl(PlanNod
                 child = PlanNodeBase::createPlanNode(rule_context.context->nextNodeId(), std::move(mark_distinct_step), PlanNodes{child});
             }
 
-            DataTypes data_types = agg_desc.function->getArgumentTypes();
+            // remove the distinct flag and set the distinct marker
+            String fun_remove_distinct = distinct_func_normal_func.at(Poco::toLower(agg_desc.function->getName()));
+            Names argument_names;
+            DataTypes data_types;
+            if (fun_remove_distinct == "countIf" && agg_desc.argument_names.size() > 1)
+            {
+                // countDistinct(arg1, arg2) cannot convert to count(arg1, arg2), because clickhousedon't support count multi arguments.
+                // As an alternative we can rewrite it to count(IF(arg1 is null, null, arg2 is null, null, 1)),
+                // or sum((arg1 is not null) AND (arg2 is not null))
+                fun_remove_distinct = "sumIf";
+
+                ASTs argument_functions;
+                for (const auto & argument : agg_desc.argument_names)
+                    argument_functions.emplace_back(makeASTFunction("isNotNull", makeASTIdentifier(argument)));
+                auto new_argument = PredicateUtils::combineConjuncts(argument_functions);
+                auto new_argument_name = rule_context.context->getSymbolAllocator()->newSymbol(new_argument);
+                new_argument_assignments.emplace_back(new_argument_name, new_argument);
+
+                argument_names.emplace_back(new_argument_name);
+                data_types.emplace_back(std::make_shared<DataTypeUInt8>());
+            }
+            else
+            {
+                argument_names = agg_desc.argument_names;
+                data_types = agg_desc.function->getArgumentTypes();
+            }
+            
+            argument_names.emplace_back(marker);
             data_types.emplace_back(std::make_shared<DataTypeUInt8>());
 
             Array parameters = agg_desc.function->getParameters();
             AggregateFunctionProperties properties;
-
-            // remove the distinct flag and set the distinct marker
-            String fun_remove_distinct = distinct_func_normal_func.at(Poco::toLower(agg_desc.function->getName()));
             AggregateFunctionPtr new_agg_fun
                 = AggregateFunctionFactory::instance().get(fun_remove_distinct, data_types, parameters, properties);
-            Names argument_names = agg_desc.argument_names;
-            argument_names.emplace_back(marker);
 
             AggregateDescription new_agg_desc;
 
@@ -143,6 +174,22 @@ TransformResult MultipleDistinctAggregationToMarkDistinct::transformImpl(PlanNod
         {
             new_agg_descs.emplace_back(agg_desc);
         }
+    }
+
+    if (!new_argument_assignments.empty())
+    {
+        NameToType name_to_type;
+        for (const auto & assignment : new_argument_assignments)
+            name_to_type.emplace(assignment.first, std::make_shared<DataTypeUInt8>());
+
+        for (const auto & input : child->getStep()->getOutputStream().header)
+        {
+            new_argument_assignments.emplace(input.name, makeASTIdentifier(input.name));
+            name_to_type.emplace(input.name, input.type);
+        }
+        auto new_argument_projection_step
+            = std::make_shared<ProjectionStep>(child->getStep()->getOutputStream(), new_argument_assignments, name_to_type);
+        child = PlanNodeBase::createPlanNode(rule_context.context->nextNodeId(), std::move(new_argument_projection_step), {child});
     }
 
     auto count_agg_step = std::make_shared<AggregatingStep>(
