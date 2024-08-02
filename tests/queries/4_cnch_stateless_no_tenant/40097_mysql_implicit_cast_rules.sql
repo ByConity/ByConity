abcd4adfe0e8040114d75db2750ad8201837d99e
fredwang <fredwang@bytedance.com>
2024-08-02 08:15:11 +0000

    Merge 'optimizer_respect_mysql_implicit_cast_rules_cnch-2.2' into 'cnch-2.2'
    fix(clickhousech@m-4052801080): optimizer respect mysql implicit cast rules cnch 2.2
    
    See merge request: !23364

---
+++ tests/queries/4_cnch_stateless_no_tenant/40097_mysql_implicit_cast_rules.sql
@@ -0,0 +1,9 @@
+SELECT 1 as a UNION ALL SELECT '1' as a; -- { serverError 386 }
+SELECT count() FROM (SELECT 1 as a) JOIN (SELECT '1' as a) USING a; -- { serverError 53 }
+SELECT count() FROM (SELECT 1 as a) t1 JOIN (SELECT '1' as a) t2 ON t1.a = t2.a; -- { serverError 53 }
+
+SET dialect_type = 'MYSQL';
+
+SELECT 1 as a UNION ALL SELECT '1' as a FORMAT Null;
+SELECT count() FROM (SELECT 1 as a) JOIN (SELECT '1' as a) USING a FORMAT Null;
+SELECT count() FROM (SELECT 1 as a) t1 JOIN (SELECT '1' as a) t2 ON t1.a = t2.a FORMAT Null;
