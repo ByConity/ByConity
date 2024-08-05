7554a0d696a859294fb2f1b1b74c016874a34546
fredwang <fredwang@bytedance.com>
2024-08-05 02:46:25 +0000

    Merge 'fix_get_json_object_for_lc' into 'cnch-2.2'
    fix(clickhousech@m-3000759214):fix lc(nullable(string)) as get_json_object arg
    
    See merge request: !23154

--- tests/queries/4_cnch_stateless/12294_function_get_json_object.sql
+++ tests/queries/4_cnch_stateless/12294_function_get_json_object.sql
@@ -5,6 +5,9 @@ SELECT JSONExtractRaw('{"n_s" : [{"ac":"abc","xz":"xz"}, {"def":"def"}], "n_i" :
 SELECT JSONExtractRaw('{"n_s" : [{"ac":"abc","xz":"xz"}, {"def":"def"}], "n_i" : [1, 23]}', 'n_s', 1);
 SELECT JSONExtractRaw('{"n_s" : [{"ac":"abc","xz":"xz"}, {"def":"def"}], "n_i" : [1, 23]}', 'n_s');
 
+select get_json_object('{"a":100}'::Nullable(String), '$.a');
+select get_json_object('{"a":100}'::LowCardinality(Nullable(String)), '$.a');
+
 DROP TABLE IF EXISTS test.test;
 CREATE TABLE test.test(a Nullable(String)) ENGINE = CnchMergeTree ORDER BY tuple() PARTITION BY tuple();
 SELECT get_json_object('{"test": "test"}', a) FROM test.test;
