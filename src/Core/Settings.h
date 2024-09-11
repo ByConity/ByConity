770b8c91947701e7360e74d69a2cbf1f0ce76821
fredwang <fredwang@bytedance.com>
2024-09-11 09:30:49 +0000

    Merge 'feat-read-in-partition-order-cnch-dev' into 'cnch-dev'
    perf(clickhousech@m-4731388683): add optimize_read_in_partition_order to improve topn
    
    See merge request: !23389
    # Conflicts:
    #	src/QueryPlan/ReadFromMergeTree.cpp

--- src/Core/Settings.h
+++ src/Core/Settings.h
@@ -871,6 +871,8 @@ enum PreloadLevelSettings : UInt64
       "longest one.", \
       0) \
     M(Bool, optimize_read_in_order, true, "Enable ORDER BY optimization for reading data in corresponding order in MergeTree tables.", 0) \
+    M(Bool, optimize_read_in_partition_order, false, "In optimize_read_in_order mode, whether to read parts partition-by-partition if applicable", 0) \
+    M(Bool, force_read_in_partition_order, 0, "Similar to optimize_read_in_partition_order, but throw an exception if it cannot be applied to the query, mainly for testing", 0) \
     M(Bool, optimize_aggregation_in_order, false, "Enable GROUP BY optimization for aggregating data in corresponding order in MergeTree tables.", 0) \
     M(UInt64, read_in_order_two_level_merge_threshold, 100, "Minimal number of parts to read to run preliminary merge step during multithread reading in order of primary key.", 0) \
     M(Bool, low_cardinality_allow_in_native_format, true, "Use LowCardinality type in Native format. Otherwise, convert LowCardinality columns to ordinary for select query, and convert ordinary columns to required LowCardinality for insert query.", 0) \
