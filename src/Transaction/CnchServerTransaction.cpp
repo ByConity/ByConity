19b9e9de3e95a03d852214f052fff2dadf319423
fredwang <fredwang@bytedance.com>
2024-10-07 06:59:02 +0000

    fix(clickhousech@m-4087770190): [cp]try capture bytekv lock timeout when write dict table
    See merge request: !24158

--- src/Transaction/CnchServerTransaction.cpp
+++ src/Transaction/CnchServerTransaction.cpp
@@ -358,6 +358,7 @@ TxnTimestamp CnchServerTransaction::commit()
 
     TxnTimestamp commit_ts = global_context->getTimestamp();
     int retry = MAX_RETRY;
+    bool bitengine_dict_related{false};
     do
     {
         try
