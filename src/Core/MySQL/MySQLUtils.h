b122d23ace115c439d95dfd70f3871df900e63a6
fredwang <fredwang@bytedance.com>
2024-10-07 07:40:15 +0000

    Merge branch 'fix-mysql-datetime-type-format-dev' into 'cnch-dev'
    fix(clickhousech@m-4789458129): Change datetime behavior of mysql protocol dev
    
    See merge request dp/ClickHouse!24231

--- src/Core/MySQL/MySQLUtils.h
+++ src/Core/MySQL/MySQLUtils.h
@@ -11,7 +11,7 @@ namespace MySQLUtils
 {
 /// Splits DateTime64 column data at a certain row number into whole and fractional part
 /// Additionally, normalizes the fractional part as if it was scale 6 for MySQL compatibility purposes
-DecimalUtils::DecimalComponents<DateTime64> getNormalizedDateTime64Components(DataTypePtr data_type, ColumnPtr col, size_t row_num);
+DecimalUtils::DecimalComponents<DateTime64> getNormalizedDateTime64Components(DataTypePtr data_type, ColumnPtr col, size_t row_num, bool adapt_scale = true);
 }
 }
 }
