97cc7fcbef47aaeee0555116ff1113d4e0990857
fredwang <fredwang@bytedance.com>
2024-10-07 03:00:23 +0000

    Merge 'fix_optimizer_join_on_condition_cnchdev' into 'cnch-dev'
    fix(optimizer@m-4874825265): Do not check function type in join condition when using optimizer cnch-dev
    
    See merge request: !23965
    # Conflicts:
    #	src/Interpreters/TreeRewriter.cpp

--- src/Interpreters/TreeRewriter.cpp
+++ src/Interpreters/TreeRewriter.cpp
@@ -768,11 +768,11 @@ void collectJoinedColumns(TableJoin & analyzed_join, const ASTTableJoin & table_
     {
         bool is_asof = (table_join.strictness == ASTTableJoin::Strictness::Asof);
 
-        CollectJoinOnKeysVisitor::Data data{analyzed_join, tables[0], tables[1], aliases, is_asof, false, enable_join_on_1_equals_1, {}, {}, false, ignore_array_join_check_in_join_on_condition, context};
+        CollectJoinOnKeysVisitor::Data data{analyzed_join, tables[0], tables[1], aliases, is_asof, false, enable_join_on_1_equals_1, {}, {}, false, ignore_array_join_check_in_join_on_condition, context, {}, !context->getSettings().enable_optimizer};
         CollectJoinOnKeysVisitor(data).visit(table_join.on_expression);
         CollectJoinOnKeysMatcher::analyzeJoinOnConditions(data, table_join.kind);
 
-        if (!data.has_some && !data.is_nest_loop_join)
+        if (!data.has_some && !data.is_nest_loop_join && !context->getSettings().enable_optimizer)
             throw Exception("Cannot get JOIN keys from JOIN ON section: " + queryToString(table_join.on_expression),
                             ErrorCodes::INVALID_JOIN_ON_EXPRESSION);
         if (is_asof)
