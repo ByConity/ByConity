77b2f5fb591364331ada061431a3442b9f0668e0
fredwang <fredwang@bytedance.com>
2024-10-08 01:13:58 +0000

    Merge branch 'fix-set-sensitive' into 'cnch-dev'
    fix(clickhousech@m-5161178137): add new SET_SENSITIVE permission
    
    See merge request dp/ClickHouse!24319

--- src/Interpreters/InterpreterSetSensitiveQuery.cpp
+++ src/Interpreters/InterpreterSetSensitiveQuery.cpp
@@ -2,6 +2,7 @@
 #include <Interpreters/Context.h>
 #include <Interpreters/InterpreterSetSensitiveQuery.h>
 #include <Catalog/Catalog.h>
+#include <Access/AccessFlags.h>
 
 namespace DB
 {
@@ -10,7 +11,16 @@ namespace DB
 BlockIO InterpreterSetSensitiveQuery::execute()
 {
     const auto & ast = query_ptr->as<ASTSetSensitiveQuery &>();
-    getContext()->getCnchCatalog()->putSensitiveResource(ast.database, ast.table, ast.column, ast.target, ast.value);
+    const auto ctx = getContext();
+
+    if (ast.target == "DATABASE")
+        ctx->checkAccess(AccessType::SET_SENSITIVE, ast.database);
+    else if (ast.target == "TABLE")
+        ctx->checkAccess(AccessType::SET_SENSITIVE, ast.database, ast.table);
+    else if (ast.target == "COLUMN")
+        ctx->checkAccess(AccessType::SET_SENSITIVE, ast.database, ast.table, ast.column);
+
+    ctx->getCnchCatalog()->putSensitiveResource(ast.database, ast.table, ast.column, ast.target, ast.value);
     return {};
 }
 
