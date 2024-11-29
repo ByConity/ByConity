87e60ce4dbd297a87b663f2091b50e7597dd7036
Fred Wang <fredwang@bytedance.com>
2024-11-29 13:26:41 +0800

    fix ci test due to file exist

--- tests/queries/4_cnch_stateless/03100_backup_restore.sql
+++ tests/queries/4_cnch_stateless/03100_backup_restore.sql
@@ -5,8 +5,9 @@ DROP TABLE IF EXISTS table_restore_03100;
 CREATE TABLE table_backup_03100(`id` UInt32) ENGINE = CnchMergeTree ORDER BY id SETTINGS index_granularity = 8192;
 INSERT INTO table_backup_03100 SELECT * FROM system.numbers LIMIT 1024;
 
-BACKUP TABLE table_backup_03100 TO DISK('hdfs_disk', 'test_backup_03100/') SETTINGS async = 0, id = 'backup_task_03100';
-RESTORE TABLE table_backup_03100 AS table_restore_03100 FROM DISK('hdfs_disk', 'test_backup_03100/') SETTINGS async=0, id = 'restore_task_03100';
+SUFFIX=$RANDOM
+BACKUP TABLE table_backup_03100 TO DISK('hdfs_disk', 'test_backup_03100_${SUFFIX}/') SETTINGS async = 0, id = 'backup_task_03100';
+RESTORE TABLE table_backup_03100 AS table_restore_03100 FROM DISK('hdfs_disk', 'test_backup_03100_${SUFFIX}/') SETTINGS async=0, id = 'restore_task_03100';
 
 SELECT count() FROM table_backup_03100;
 SELECT count() FROM table_restore_03100;
@@ -24,8 +25,8 @@ INSERT INTO unique_backup_03101 SELECT * FROM system.numbers LIMIT 1024;
 delete from unique_backup_03101 where id > 50;
 delete from unique_backup_03101 where id = 1;
 
-BACKUP TABLE unique_backup_03101 TO DISK('hdfs_disk', 'test_backup_03101/') SETTINGS async = 0, id = 'backup_task_03101';
-RESTORE TABLE unique_backup_03101 AS unique_restore_03101 FROM DISK('hdfs_disk', 'test_backup_03101/') SETTINGS async=0, id = 'restore_task_03101';
+BACKUP TABLE unique_backup_03101 TO DISK('hdfs_disk', 'test_backup_03101_${SUFFIX}/') SETTINGS async = 0, id = 'backup_task_03101';
+RESTORE TABLE unique_backup_03101 AS unique_restore_03101 FROM DISK('hdfs_disk', 'test_backup_03101_${SUFFIX}/') SETTINGS async=0, id = 'restore_task_03101';
 
 SELECT count() FROM unique_backup_03101;
 SELECT count() FROM unique_restore_03101;
@@ -41,8 +42,8 @@ CREATE TABLE partition_backup_03102(`id` UInt32, `group` UInt32) ENGINE = CnchMe
 INSERT INTO partition_backup_03102 values (1, 1), (2, 1);
 INSERT INTO partition_backup_03102 values (1, 2), (2, 2);
 
-BACKUP TABLE partition_backup_03102 PARTITIONS '1' TO DISK('hdfs_disk', 'test_backup_03102/') SETTINGS async = 0, id = 'backup_task_03102';
-RESTORE TABLE partition_backup_03102 AS partition_restore_03102 PARTITIONS '1' FROM DISK('hdfs_disk', 'test_backup_03102/') SETTINGS async=0, id = 'restore_task_03102';
+BACKUP TABLE partition_backup_03102 PARTITIONS '1' TO DISK('hdfs_disk', 'test_backup_03102_${SUFFIX}/') SETTINGS async = 0, id = 'backup_task_03102';
+RESTORE TABLE partition_backup_03102 AS partition_restore_03102 PARTITIONS '1' FROM DISK('hdfs_disk', 'test_backup_03102_${SUFFIX}/') SETTINGS async=0, id = 'restore_task_03102';
 
 SELECT count() FROM partition_backup_03102;
 SELECT count() FROM partition_restore_03102;
@@ -63,8 +64,8 @@ INSERT INTO table_backup SELECT * FROM system.numbers LIMIT 128;
 CREATE TABLE unique_backup(`id` UInt32) ENGINE = CnchMergeTree UNIQUE KEY id ORDER BY id SETTINGS index_granularity = 8192;
 INSERT INTO unique_backup SELECT * FROM system.numbers LIMIT 256;
 
-BACKUP DATABASE database_backup_03103 TO DISK('hdfs_disk', 'test_backup_03103/') SETTINGS async = 0, id = 'backup_task_03103';
-RESTORE DATABASE database_backup_03103 AS database_restore_03103 FROM DISK('hdfs_disk', 'test_backup_03103/') SETTINGS async=0, id = 'restore_task_03103';
+BACKUP DATABASE database_backup_03103 TO DISK('hdfs_disk', 'test_backup_03103_${SUFFIX}/') SETTINGS async = 0, id = 'backup_task_03103';
+RESTORE DATABASE database_backup_03103 AS database_restore_03103 FROM DISK('hdfs_disk', 'test_backup_03103_${SUFFIX}/') SETTINGS async=0, id = 'restore_task_03103';
 
 SELECT count() FROM database_backup_03103.table_backup;
 SELECT count() FROM database_restore_03103.table_backup;
@@ -87,8 +88,8 @@ INSERT INTO table_backup SELECT * FROM system.numbers LIMIT 128;
 CREATE TABLE unique_backup(`id` UInt32) ENGINE = CnchMergeTree UNIQUE KEY id ORDER BY id SETTINGS index_granularity = 8192;
 INSERT INTO unique_backup SELECT * FROM system.numbers LIMIT 256;
 
-BACKUP DATABASE database_backup_03104 EXCEPT TABLES unique_backup TO DISK('hdfs_disk', 'test_backup_03104/') SETTINGS async = 0, id = 'backup_task_03104';
-RESTORE DATABASE database_backup_03104 AS database_restore_03104 FROM DISK('hdfs_disk', 'test_backup_03104/') SETTINGS async=0, id = 'restore_task_03104';
+BACKUP DATABASE database_backup_03104 EXCEPT TABLES unique_backup TO DISK('hdfs_disk', 'test_backup_03104_${SUFFIX}/') SETTINGS async = 0, id = 'backup_task_03104';
+RESTORE DATABASE database_backup_03104 AS database_restore_03104 FROM DISK('hdfs_disk', 'test_backup_03104_${SUFFIX}/') SETTINGS async=0, id = 'restore_task_03104';
 
 SELECT count() FROM database_backup_03104.table_backup;
 SELECT count() FROM database_restore_03104.table_backup;
