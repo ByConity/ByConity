6fc239961e746e4260b4635bf40d202a26dad816
Fred Wang <fredwang@bytedance.com>
2024-12-04 16:03:58 +0800

    Revert "Revert "Merge 'enable_catalog_dev' into 'cnch-dev'""
    This reverts commit 3b640da1cba5738a773c45ef4cfbdb0e57491537.

--- programs/server/Server.cpp
+++ programs/server/Server.cpp
@@ -1495,15 +1495,10 @@ int Server::main(const std::vector<std::string> & /*args*/)
         throw;
     }
 
-    // Note:: just for test.
-    {
-        // WARNING: There is a undesired restriction on FDB. Each process could only init one fdb client otherwise it will panic.
-        // so if we use fdb as the kv storage, the config for external and internal catalog must be the same.
-        if (global_context->getCnchConfigRef().has(ExternalCatalog::Mgr::configPrefix()))
-        {
-            ExternalCatalog::Mgr::init(*global_context, global_context->getCnchConfigRef());
-        }
-    }
+    // WARNING: There is a undesired restriction on FDB. Each process could only init one fdb client otherwise it will panic.
+    // so if we use fdb as the kv storage, the config for external and internal catalog must be the same.
+    ExternalCatalog::Mgr::init(*global_context, global_context->getConfigRef());
+
     /// Check sanity of MergeTreeSettings on server startup
     global_context->getMergeTreeSettings().sanityCheck(settings);
     global_context->getReplicatedMergeTreeSettings().sanityCheck(settings);
